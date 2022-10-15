7859ff0f1a576d1c2ccbf227e64e8b28c91da98e
Ollivier Robert <roberto@keltia.net>
2022-10-16 01:02:17 +0200

    This is how you disable auto-version now.

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
 
 /// CLI options
 #[derive(Parser, Debug)]
+#[command(disable_version_flag = true)]
 #[clap(name = crate_name!(), about = crate_description!())]
 #[clap(version = crate_version ! (), author = crate_authors ! ())]
 struct Opts {
@@ -30,12 +31,21 @@ struct Opts {
     /// Verbose mode
     #[clap(short = 'v', long)]
     verbose: bool,
+    #[clap(short = 'V', long)]
+    version: bool,
     /// Input file
     input: PathBuf,
 }
 
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
+
+    // Bypass
+    //
+    if opts.version {
+        println!("{}", version());
+        return Ok(());
+    }
 
     // Load default config if nothing is specified
     let cfg = get_config(opts.config);
