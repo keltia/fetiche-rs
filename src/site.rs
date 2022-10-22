7970bf7ea35f8e1c97e0babbccfd363d988bb780
Ollivier Robert <roberto@keltia.net>
2022-10-22 19:36:39 +0200

    Use an enum for Site, much clearer that way.  Add tests and functions.

--- src/site.rs
+++ src/site.rs
@@ -8,35 +8,193 @@
 //! We assume the output format to be the same with `{ access_token: String }`.
 //!
 
-use crate::format::Source;
+use anyhow::{anyhow, bail, Result};
+use log::{debug, error, info, trace};
+use serde::{Deserialize, Serialize};
+use std::fs;
+use std::path::PathBuf;
 
-use serde::Deserialize;
+use crate::format::Source;
+use crate::Config;
 
 /// Describe what a site is and associated credentials.
 ///
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(untagged)]
+pub enum Site {
+    Login {
+        /// Type of input
+        format: String,
+        /// Base URL (to avoid repeating)
+        base_url: String,
+        /// Token fetching URL (if present call this first)
+        token: String,
+        /// Data fetching URL
+        get: String,
+        /// Login (if needed)
+        login: String,
+        /// Password if needed
+        password: String,
+    },
+    Anon {
+        /// Type of input
+        format: String,
+        /// Base URL (to avoid repeating)
+        base_url: String,
+        /// Data fetching URL
+        get: String,
+    },
+    Invalid,
+}
+
+/// Access token derived from username/password
+///
 #[derive(Debug, Deserialize)]
-pub struct Site {
-    /// Type of input
-    #[serde(rename = "type")]
-    pub stype: Source,
-    /// Base URL (to avoid repeating)
-    pub base_url: String,
-    /// Token fetching URL (if present call this first)
-    pub token: Option<String>,
-    /// Data fetching URL
-    pub get: String,
-    /// Login (if needed)
-    pub login: Option<String>,
-    /// Password if needed
-    pub password: Option<String>,
+struct Token {
+    /// Token (SHA-256 or -512 data I guess)
+    access_token: String,
+}
+
+impl Site {
+    /// Initialize a site by checking whether it is present in the configuration file
+    ///
+    pub fn new(cfg: &Config, site: &str) -> Self {
+        if cfg.sites.contains_key(site) {
+            return cfg.sites[site].clone();
+        }
+        error!("{site} not found!");
+        Site::Invalid
+    }
+
+    pub fn format(&self) -> Source {
+        match self {
+            Site::Login { format, .. } | Site::Anon { format, .. } => Source::from_str(format),
+            _ => Source::None,
+        }
+    }
+
+    /// Fetch the access token linked to the given login/password
+    ///
+    fn fetch_token(&self, client: &reqwest::blocking::Client) -> Result<String> {
+        match self {
+            Site::Login {
+                base_url,
+                login,
+                password,
+                token,
+                ..
+            } => {
+                // Prepare our data
+                //
+                let body = format!(
+                    "{{\"username\": \"{}\", \"password\": \"{}\"}}",
+                    login, password
+                );
+
+                // fetch token
+                //
+                let url = format!("{}{}", base_url, token);
+                trace!("Fetching token through {}…", url);
+                let resp = client
+                    .post(url)
+                    .header("content-type", "application/json")
+                    .body(body)
+                    .send();
+
+                let resp = resp?.text()?;
+
+                let res: Token = serde_json::from_str(&resp)?;
+                debug!("{:?}", res);
+                Ok(res.access_token)
+            }
+            _ => return Err(anyhow!("no credential needed")),
+        }
+    }
+
+    /// Using the access token obtained through `fetch_token()`, fetch the given CSV data
+    ///
+    pub fn fetch(&self) -> Result<String> {
+        info!("Fetch data from network…");
+
+        let client = reqwest::blocking::Client::new();
+        let resp = match self {
+            Site::Login { base_url, get, .. } => {
+                // First call to gen auth token
+                //
+                let token = &self.fetch_token(&client)?;
+
+                // Use the token to authenticate ourselves
+                //
+                let url = format!("{}{}", base_url, get);
+                client
+                    .get(url)
+                    .header("content-type", "application/json")
+                    .header("Authorization", format!("Bearer {}", token))
+                    .send()
+            }
+            Site::Anon { base_url, get, .. } => {
+                let url = format!("{}{}", base_url, get);
+                client
+                    .get(url)
+                    .header("content-type", "application/json")
+                    .send()
+            }
+            Site::Invalid => panic!("nope"),
+        };
+
+        match resp {
+            Ok(resp) => Ok(resp.text().unwrap()),
+            Err(e) => bail!("HTTP error: {}", e),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashMap;
 
     use crate::Config;
     use std::path::PathBuf;
+    use toml::to_string;
+
+    fn set_default() -> Config {
+        let s = Site::Anon {
+            format: "aeroscope".to_string(),
+            base_url: "http://example.net/".to_string(),
+            get: "/get".to_string(),
+        };
+
+        let mut h: HashMap<String, Site> = HashMap::new();
+        h.insert("foo".to_string(), s);
+
+        let cfg = Config {
+            default: "none".to_string(),
+            sites: h,
+        };
+
+        cfg
+    }
+
+    #[test]
+    fn test_site_new() {
+        let cfg = set_default();
+        dbg!(toml::to_string(&cfg).unwrap());
+
+        let s = Site::new(&cfg, "foo");
+        match s {
+            Site::Anon {
+                format,
+                base_url,
+                get,
+            } => {
+                assert_eq!("aeroscope", format);
+                assert_eq!("http://example.net/", &base_url);
+                assert_eq!("/get", &get);
+            }
+            _ => (),
+        }
+    }
 
     #[test]
     fn test_site_loading() {
@@ -44,16 +202,31 @@ mod tests {
         let cfg = Config::load(&cfn);
         assert!(cfg.is_ok());
 
-        let s = &cfg.unwrap();
-        assert_eq!("none", s.default);
-        assert!(!s.sites.is_empty());
-        assert!(3, s.len());
-        assert!(s.sites.contains_key("none"));
+        let cfg = cfg.unwrap();
+        let s = cfg.sites;
 
-        assert!(s.sites["none"].login.is_none());
-        assert!(Source::None, s.sites["none"].stype);
+        assert_eq!("none", cfg.default);
+        assert!(!s.is_empty());
+        assert_eq!(3, s.len());
+        assert!(s.contains_key("nope"));
 
-        assert!(Source::Aeroscope, s.sites["aeroscope"].stype);
-        assert!("SOMETHING", s.sites["aeroscope"].login);
+        for (_, s) in s.iter() {
+            match s {
+                Site::Anon { format, .. } => {
+                    assert_eq!("safesky", format);
+                }
+                Site::Login {
+                    format,
+                    base_url,
+                    token,
+                    ..
+                } => {
+                    assert_ne!("none", format);
+                    assert!(!base_url.is_empty());
+                    assert!(!token.is_empty());
+                }
+                Site::Invalid => panic!("nope"),
+            }
+        }
     }
 }
