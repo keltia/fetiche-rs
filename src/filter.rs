5b5bb110ed9f2b6cc964d49c36819bd33d924bea
Ollivier Robert <roberto@keltia.net>
2022-11-04 17:20:03 +0100

    Fix  date manipulations in filters.  Switch to NaiveDateTime.

--- src/filter.rs
+++ src/filter.rs
@@ -1,15 +1,15 @@
 //! submodule to manage date filters
 //!
 
-use chrono::{DateTime, Local};
+use chrono::{Local, NaiveDateTime};
 
 /// If we specify -B/-E or --today, we need to pass these below
 ///
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Filter {
     Interval {
-        begin: DateTime<Local>,
-        end: DateTime<Local>,
+        begin: NaiveDateTime,
+        end: NaiveDateTime,
     },
     None,
 }
@@ -25,7 +25,7 @@ impl Default for Filter {
 impl Filter {
     /// from two time points
     ///
-    pub fn from(begin: DateTime<Local>, end: DateTime<Local>) -> Self {
+    pub fn from(begin: NaiveDateTime, end: NaiveDateTime) -> Self {
         Filter::Interval { begin, end }
     }
 }
@@ -40,10 +40,12 @@ mod tests {
         let begin = "2022-11-11 12:34:56";
         let end = "2022-11-30 12:34:56";
 
-        let begin = begin.parse::<DateTime<Local>>()?;
-        let end = end.parse::<DateTime<Local>>()?;
+        let begin = NaiveDateTime::parse_from_str(begin, "%Y-%m-%d %H:%M:%S");
+        assert!(begin.is_ok());
+        let end = NaiveDateTime::parse_from_str(end, "%Y-%m-%d %H:%M:%S");
+        assert!(end.is_ok());
 
-        let f = Filter::from(begin, end);
+        let f = Filter::from(begin.unwrap(), end.unwrap());
         assert_ne!(Filter::None, f);
         Ok(())
     }
