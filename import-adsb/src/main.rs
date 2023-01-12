1146c4d862e33b4ee2320bda8bd3991bf8b9d122
Ollivier Robert <roberto@keltia.net>
2023-01-12 23:35:38 +0100

    Display banner.

--- import-adsb/src/main.rs
+++ import-adsb/src/main.rs
@@ -1,5 +1,13 @@
+use crate::cli::Opts;
+use crate::version::version;
+
+use clap::Parser;
+
 mod cli;
+mod version;
 
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    println!("{}", version());
 }
