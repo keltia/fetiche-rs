5d3565898f767daa787a179b56393fa642c6ad93
Ollivier Robert <roberto@keltia.net>
2023-03-15 13:24:27 +0100

    Typo.

--- sites/src/config.rs
+++ sites/src/config.rs
@@ -220,7 +220,7 @@ impl<'a> IntoIterator for &'a Sites {
     type Item = (&'a String, &'a Site);
     type IntoIter = Iter<'a, String, Site>;
 
-    /// We can now to `sites.iter()`
+    /// We can now do `sites.iter()`
     ///
     fn into_iter(self) -> Iter<'a, String, Site> {
         self.site.iter()
