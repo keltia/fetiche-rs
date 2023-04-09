dd36b879be60edc781135761af5e64d3a417e772
Ollivier Robert <roberto@keltia.net>
2023-04-09 17:54:43 +0200

    Move ASTERIX specific const as well.

--- format-specs/src/asterix/mod.rs
+++ format-specs/src/asterix/mod.rs
@@ -1,5 +1,15 @@
+//! Asterix specific module
+//!
+//! Regroup the various pseudo-categories we can use.
+//!
+
 mod cat129;
 mod cat21;
 
 pub use cat129::*;
 pub use cat21::*;
+
+/// Default SAC: France
+pub const DEF_SAC: usize = 8;
+/// Default SIC
+pub const DEF_SIC: usize = 200;
