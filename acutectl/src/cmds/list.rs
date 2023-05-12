c69ebbce408a94e2852372a8718c44860bb8790e
Ollivier Robert <roberto@keltia.net>
2023-05-12 19:57:02 +0200

    Add list_formats.

--- acutectl/src/cmds/list.rs
+++ acutectl/src/cmds/list.rs
@@ -1,13 +1,19 @@
 use std::path::PathBuf;
 
 use anyhow::Result;
+use format_specs::Format;
 
 use sources::Sites;
 
+/// Fetch the list of supported formats and their description.
+///
 pub fn list_formats() -> Result<String> {
-    Ok("None".to_owned())
+    let str = Format::list()?;
+    Ok(str)
 }
 
+/// Fetch all the different sources available.
+///
 pub fn list_sources(cfg: &Sites) -> Result<String> {
     let str = cfg
         .iter()
