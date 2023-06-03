7882fd85a1d50e650518697ebf618ad550fe3120
Ollivier Robert <roberto@keltia.net>
2023-06-03 12:27:12 +0200

    Return package version.

--- sources/src/lib.rs
+++ sources/src/lib.rs
@@ -61,3 +61,10 @@ const BASEDIR: &str = ".config";
 
 /// Relative path to `BASEDIR` for storing auth tokens
 const TOKEN_BASE: &str = "tokens";
+
+const SOURCE_NAME: &str = env!("CARGO_PKG_NAME");
+const SOURCE_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub fn version() -> String {
+    format!("{}/{}", SOURCE_NAME, SOURCE_VERSION)
+}
