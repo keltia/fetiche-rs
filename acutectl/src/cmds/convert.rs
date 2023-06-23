8a1aacec0e6916d38e9bf04b6e79fffb4f03d549
Ollivier Robert <roberto@keltia.net>
2023-06-23 12:05:52 +0200

    Add "convert" command skeleton.

---
+++ acutectl/src/cmds/convert.rs
@@ -0,0 +1,12 @@
+use anyhow::Result;
+use log::trace;
+
+use fetiche_engine::Engine;
+
+use crate::ConvertOpts;
+
+pub fn convert_from_to(engine: &Engine, copts: &ConvertOpts) -> Result<()> {
+    trace!("convert_from_to");
+
+    Ok(())
+}
