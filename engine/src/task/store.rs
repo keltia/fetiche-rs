a21bbfc3924409b94f352f65f1e59b1453d9fddf
Ollivier Robert <roberto@keltia.net>
2023-06-26 11:19:45 +0200

    Implement Store fully.

--- engine/src/task/store.rs
+++ engine/src/task/store.rs
@@ -4,11 +4,13 @@
 //! 2. store all data coming from the pipe in files every hour
 //!
 
-use std::fs::create_dir;
+use std::fs::{create_dir, File, OpenOptions};
+use std::io::Write;
 use std::path::PathBuf;
 use std::sync::mpsc::Sender;
 
 use anyhow::Result;
+use chrono::{Datelike, Utc};
 use log::trace;
 
 use engine_macros::RunnableDerive;
@@ -29,7 +31,7 @@ pub struct Store {
 impl Default for Store {
     fn default() -> Self {
         Store {
-            io: IO::In,
+            io: IO::Consumer,
             id: "".to_string(),
             path: None,
         }
@@ -38,19 +40,54 @@ impl Default for Store {
 
 impl Store {
     pub fn new(path: &str, id: &str) -> Self {
-        let path = makepath!(path, id);
+        let path: PathBuf = makepath!(path, id);
 
-        create_dir(path).expect(&format!("can not create {} in {}", id, path));
+        // Base is PATH/ID/
+        //
+        create_dir(&path).expect(&format!(
+            "can not create {} in {}",
+            id,
+            path.to_string_lossy()
+        ));
 
+        trace!("store::new({})", path.to_string_lossy());
         Store {
-            io: IO::In,
+            io: IO::Consumer,
             id: id.to_owned(),
             path: Some(path.clone()),
         }
     }
 
     /// Store is like
+    ///
     pub fn execute(&mut self, data: String, stdout: Sender<String>) -> Result<()> {
-        Ok(())
+        trace!("store::execute");
+
+        let tm = Utc::now();
+
+        // Extract parts to get to a filename
+        //
+        let (year, month, day) = (tm.year(), tm.month(), tm.day());
+        let fname = format!("{}{}{}-000000", year, month, day);
+
+        // Full path is BASE/ID/FNAME
+        //
+        let base: PathBuf = [self.path.clone().unwrap(), PathBuf::from(&self.id)]
+            .iter()
+            .collect();
+        let path: PathBuf = [base, PathBuf::from(fname)].iter().collect();
+
+        // Create if not present
+        //
+        if !path.exists() {
+            File::create(&path)?;
+        }
+
+        // Append to it
+        //
+        let mut fh = OpenOptions::new().write(true).append(true).open(&path)?;
+        write!(fh, "{}", data)?;
+
+        Ok(stdout.send("w".to_string())?)
     }
 }
