19ebcec909f61761d2363a7bcd56df581c6e37e7
Ollivier Robert <roberto@keltia.net>
2023-07-03 16:56:07 +0200

    Structs for reading configuration.

---
+++ engine/src/config.rs
@@ -0,0 +1,24 @@
+use std::collections::BTreeMap;
+use std::path::PathBuf;
+
+use serde::Deserialize;
+
+/// Configuration file format
+///
+#[derive(Clone, Debug, Deserialize)]
+pub struct EngineConfig {
+    /// Usual check for malformed file
+    pub version: usize,
+    /// List of storage types
+    pub storage: BTreeMap<String, StorageConfig>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub enum StorageConfig {
+    /// S3 AWS/Garage bucket
+    Bucket { region: String, name: String },
+    /// in-memory K/V store like DragonflyDB or REDIS
+    Cache { url: String },
+    /// In the local filesystem
+    Directory { path: PathBuf },
+}
