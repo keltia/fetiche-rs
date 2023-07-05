f6e5fc196dba66eb296256eea97ebf4d37e46619
Ollivier Robert <roberto@keltia.net>
2023-07-05 23:10:59 +0200

    Change Fetchable to be like Streamable with Sender and not Write.

--- sources/src/lib.rs
+++ sources/src/lib.rs
@@ -7,7 +7,6 @@
 //!
 
 use std::fmt::{Debug, Display, Formatter};
-use std::io::Write;
 use std::sync::mpsc::Sender;
 
 use anyhow::Result;
@@ -65,7 +64,7 @@ pub trait Fetchable: Debug {
     /// If credentials are needed, get a token for subsequent operations
     fn authenticate(&self) -> Result<String>;
     /// Fetch actual data
-    fn fetch(&self, out: &mut dyn Write, token: &str, args: &str) -> Result<()>;
+    fn fetch(&self, out: Sender<String>, token: &str, args: &str) -> Result<()>;
     /// Returns the input formats
     fn format(&self) -> Format;
 }
