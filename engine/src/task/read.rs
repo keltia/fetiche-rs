c23e7a0d502e4a68e714a7ce2ed1eacc3e3b2bd9
Ollivier Robert <roberto@keltia.net>
2023-07-11 00:22:17 +0200

    Back this out, incorrect.

--- engine/src/task/read.rs
+++ engine/src/task/read.rs
@@ -84,7 +84,7 @@ impl Read {
 
             // Now send each line down the pipe
             //
-            bfh.lines().for_each(|l| stdout.send(l.unwrap())?);
+            bfh.lines().for_each(|l| stdout.send(l.unwrap()).unwrap());
 
             Ok(())
         }
