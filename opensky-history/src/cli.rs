5d6c5a9143e0ff7c161936bb50cc7d09c7064337
Ollivier Robert <roberto@keltia.net>
2023-07-28 10:37:55 +0200

    Reorder and add "-R" for the detection range.

--- opensky-history/src/cli.rs
+++ opensky-history/src/cli.rs
@@ -8,12 +8,15 @@ pub struct Opts {
     /// Location file path
     #[clap(short = 'C', long)]
     pub config: Option<String>,
-    /// Output file (default is stdout).
-    #[clap(short = 'o', long)]
-    pub output: Option<String>,
     /// Location name (if in `locations.hcl`).
     #[clap(short = 'n', long)]
     pub name: Option<String>,
+    /// Output file (default is stdout).
+    #[clap(short = 'o', long)]
+    pub output: Option<String>,
+    /// Detection range in nautical miles (default is 25)
+    #[clap(short = 'R', long, default_value = 25)]
+    pub range: u32,
     /// Location latitude
     pub lat: Option<f32>,
     /// Location longitude
