fab4947ff1d47e9e49ed6256ca10c9e522ba104e
Ollivier Robert <roberto@keltia.net>
2023-08-10 16:02:25 +0200

    Check proxy and use openssl.

--- sources/examples/tls.rs
+++ sources/examples/tls.rs
@@ -4,7 +4,8 @@
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
-use native_tls::TlsConnector;
+use openssl::ssl::{SslConnector, SslMethod};
+use reqwest::Url;
 use tracing::trace;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::{fmt, EnvFilter};
@@ -25,14 +26,27 @@ fn main() -> eyre::Result<()> {
     //
     tracing_subscriber::registry().with(filter).with(fmt).init();
 
-    //let proxy = "proxysrv.eurocontrol.fr:8080";
+    let proxy = std::env::var("http_proxy")?;
 
-    let connector = TlsConnector::new()?;
-    let stream = TcpStream::connect(format!("{}:{}", URL, PORT))?;
+    let connector = SslConnector::builder(SslMethod::tls())?.build();
+    let stream = if proxy.is_empty() {
+        trace!("no proxy");
+        TcpStream::connect(format!("{}:{}", URL, PORT))?
+    } else {
+        trace!("using proxy");
+
+        let url = Url::parse(&proxy)?;
+        let (host, port) = (url.host().unwrap(), url.port().unwrap());
+
+        trace!("proxy = {}:{}", host, port);
+
+        let mut stream = TcpStream::connect(format!("{}:{}", host, port))?;
+        stream.write_all(format!("CONNECT {}:{} HTTP/1.1\r\n\r\n", URL, PORT).as_bytes())?;
+        stream
+    };
     let mut stream = connector.connect(URL, stream)?;
 
-    let str = format!("GET /\r\nHost: {}\r\nConnection: close\r\n\r\n", URL,);
-    stream.write_all(str.as_bytes())?;
+    let str = format!("GET /\r\nHost: {}\r\nConnection: close\r\n\r\n", URL);
 
     trace!("read from");
     let mut res = String::new();
