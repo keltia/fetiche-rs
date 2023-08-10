986874026261661a08cafc0bb836007665706e9d
Ollivier Robert <roberto@keltia.net>
2023-08-10 14:52:49 +0200

    Add native_tls example.

---
+++ sources/examples/tls.rs
@@ -0,0 +1,46 @@
+//! No proxy
+//!
+
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+use native_tls::TlsConnector;
+use tracing::trace;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
+const URL: &str = "www.whatismyip.com:";
+const PORT: u16 = 443;
+
+fn main() -> eyre::Result<()> {
+    trace!("open connection");
+
+    let fmt = fmt::layer().with_target(false).compact();
+
+    // Load filters from environment
+    //
+    let filter = EnvFilter::from_default_env();
+
+    // Combine filter & specific format
+    //
+    tracing_subscriber::registry().with(filter).with(fmt).init();
+
+    let proxy = "proxysrv.eurocontrol.fr:8080";
+
+    let connector = TlsConnector::new()?;
+    let stream = TcpStream::connect(proxy)?;
+    let mut stream = connector.connect("eurocontrol.fr", stream)?;
+
+    let str = format!(
+        "GET https://{}:{}/\r\nHost: {}\r\nConnection: close\r\n\r\n",
+        URL, PORT, URL
+    );
+    stream.write_all(str.as_bytes())?;
+
+    trace!("read from");
+    let mut res = String::new();
+    stream.read_to_string(&mut res)?;
+
+    eprintln!("IP={}", res);
+    Ok(())
+}
