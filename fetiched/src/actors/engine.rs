67004a9a237dcec5a1081f2af5ffab8222e93209
Ollivier Robert <roberto@keltia.net>
2023-09-10 23:46:05 +0200

    Add more data to the GetStatus answer.

--- fetiched/src/actors/engine.rs
+++ fetiched/src/actors/engine.rs
@@ -1,18 +1,44 @@
 //! This `Actor` wraps the `Engine` from `fetiche-engine` and will provide an interface to it.
 //!
 
+use actix::dev::{MessageResponse, OneshotSender};
 use actix::prelude::*;
 use tracing::info;
 
 use fetiche_engine::Engine;
 
+// ---- Commands
+
 #[derive(Debug, Message)]
-#[rtype(result = "()")]
-pub struct EngineStatus {}
+#[rtype(result = "EngineStatus")]
+pub struct GetStatus {}
+
+#[derive(Debug, Message)]
+#[rtype(result = "EngineStatus")]
+pub struct EngineStatus {
+    /// Runtime working area
+    pub home: String,
+    /// Number of jobs curretnly in queue
+    pub jobs: usize,
+}
+
+impl<A, M> MessageResponse<A, M> for EngineStatus
+where
+    A: Actor,
+    M: Message<Result = EngineStatus>,
+{
+    fn handle(self, _ctx: &mut A::Context, tx: Option<OneshotSender<M::Result>>) {
+        if let Some(tx) = tx {
+            tx.send(self);
+        }
+    }
+}
 
 #[derive(Debug, Message)]
 #[rtype(result = "String")]
-pub struct EngineVersion {}
+pub struct GetVersion {}
+
+// ----- The Actor
 
 #[derive(Debug)]
 pub struct EngineActor {
@@ -39,20 +65,25 @@ impl Actor for EngineActor {
     }
 }
 
-impl Handler<EngineStatus> for EngineActor {
-    type Result = ();
+impl Handler<GetStatus> for EngineActor {
+    type Result = EngineStatus;
 
     #[tracing::instrument(skip(self, msg))]
-    fn handle(&mut self, msg: EngineStatus, _: &mut Self::Context) -> Self::Result {
+    fn handle(&mut self, msg: GetStatus, _: &mut Self::Context) -> Self::Result {
         info!("{} {}", "EngineActor", fetiche_engine::version());
+
+        EngineStatus {
+            home: self.e.home.to_owned().to_string_lossy().to_string(),
+            jobs: self.e.jobs.read().iter().len(),
+        }
     }
 }
 
-impl Handler<EngineVersion> for EngineActor {
+impl Handler<GetVersion> for EngineActor {
     type Result = String;
 
     #[tracing::instrument(skip(self, msg))]
-    fn handle(&mut self, msg: EngineVersion, _: &mut Self::Context) -> Self::Result {
+    fn handle(&mut self, msg: GetVersion, _: &mut Self::Context) -> Self::Result {
         fetiche_engine::version()
     }
 }
