c67cad2d97396bef5f06482563af98737fa2c0a9
Ollivier Robert <roberto@keltia.net>
2023-09-11 16:25:26 +0200

    Just wanted to check how to use EnumString/strum::Display.

---
+++ engine/examples/enum.rs
@@ -0,0 +1,21 @@
+use eyre::Result;
+use strum::{EnumString, EnumVariantNames};
+
+use crate::Cmd::Complex;
+
+#[derive(Debug, strum::Display, EnumVariantNames, EnumString)]
+enum Cmd {
+    Bare,
+    String(String),
+    Complex { foo: usize },
+}
+
+fn main() -> Result<()> {
+    let a = Cmd::Bare;
+    let b = Cmd::String("here is a string".to_string());
+    let c = Complex { foo: 42 };
+
+    println!("a={a} b={b} c={c}");
+
+    Ok(())
+}
