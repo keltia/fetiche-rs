14142334eca36a047b3065c0a64b78e5445e0cf2
Ollivier Robert <roberto@keltia.net>
2023-09-12 17:14:21 +0200

    Add global vars, will move.

--- fetiched/src/lib.rs
+++ fetiched/src/lib.rs
@@ -1,3 +1,8 @@
 pub use actors::*;
 
 mod actors;
+
+/// Main state data file, will be created in `basedir`.
+pub(crate) const STATE_FILE: &str = "state";
+
+pub(crate) const DEF_HOMEDIR: &str = "/var/run/fetiche";
