07f65d7997053eec0b804acf4d4e5f00b3809350
Ollivier Robert <roberto@keltia.net>
2023-09-13 14:52:05 +0200

    Use a str, show diff with Debug.

--- engine/examples/enum.rs
+++ engine/examples/enum.rs
@@ -2,18 +2,18 @@ use eyre::Result;
 use strum::{EnumString, EnumVariantNames};
 
 #[derive(Debug, strum::Display, EnumVariantNames, EnumString)]
-enum Cmd {
+enum Cmd<'a> {
     Bare,
-    String(String),
+    String(&'a str),
     Complex { foo: usize },
 }
 
 fn main() -> Result<()> {
     let a = Cmd::Bare;
-    let b = Cmd::String("here is a string".to_string());
+    let b = Cmd::String("here is a string");
     let c = Cmd::Complex { foo: 42 };
 
-    println!("a={a} b={b} c={c}");
+    println!("a={a} b={b} c={c} c={c:?}");
 
     Ok(())
 }
