c3dec260317ada18f7e99ea8b0741609d500029e
Ollivier Robert <roberto@keltia.net>
2023-09-19 17:22:32 +0200

    Still working on the language.

--- engine/src/parse.rs
+++ engine/src/parse.rs
@@ -1,26 +1,16 @@
 //! Compiler for the Fetiche job language
 //!
-//! ```text
-//! ## Description of the job & task language
+//! Description of the job & task language
 //!
 //! >NOTE: Highly subject to changes
 //!
 //! ```text
 //! job "Fetch Opensky data" is
-//!     schedule every(5mn) | at(DATE)[,at(DATE)]*  // ?
-//!     
-//!     message "Beginning"
-//!
-//!     fetch("opensky")
-//!     
-//!     message "Transform into Cat21"
-//!     
-//!     into(Cat21)
-//!     
-//!     output("aeroscope.csv")
+//!     fetch "opensky"
+//!     into Cat21
+//!     output "aeroscope.csv"
 //! end
 //! ```
-//!
 
 use nom::{
     bytes::complete::{tag, take_until},
