8446e2302fbebf536d0a3cd9dc9a00a8e94dbbaa
Ollivier Robert <roberto@keltia.net>
2023-09-28 16:58:38 +0200

    Trying to store actor's mailbox addr.

--- fetiched/src/actors/config/core.rs
+++ fetiched/src/actors/config/core.rs
@@ -1,17 +1,21 @@
 use std::str::FromStr;
 
 use actix::dev::{MessageResponse, OneshotSender};
-use actix::{Actor, Message};
+use actix::{Actor, Addr, Message};
 use serde::Serialize;
 use strum::EnumVariantNames;
 
 #[derive(Clone, Debug, strum::Display, EnumVariantNames, Serialize)]
-pub enum Param {
+pub enum Param<A>
+where
+    A: Actor,
+{
+    Addr(Addr<A>),
     Integer(i32),
     String(String),
 }
 
-impl FromStr for Param {
+impl<T> FromStr for Param<T> {
     type Err = ();
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
@@ -19,22 +23,31 @@ impl FromStr for Param {
     }
 }
 
-impl From<i32> for Param {
+impl<T> From<i32> for Param<T> {
     fn from(value: i32) -> Self {
         Param::Integer(value)
     }
 }
 
-impl From<u32> for Param {
+impl<T> From<u32> for Param<T> {
     fn from(value: u32) -> Self {
         Param::Integer(value as i32)
     }
 }
 
-impl<A, M> MessageResponse<A, M> for Param
+impl<A> From<Addr<A>> for Param<A>
 where
     A: Actor,
-    M: Message<Result = Param>,
+{
+    fn from(value: Addr<A>) -> Self {
+        Param::Addr(value)
+    }
+}
+
+impl<A, M, T> MessageResponse<A, M> for Param<T>
+where
+    A: Actor,
+    M: Message<Result = Param<T>>,
 {
     fn handle(self, _ctx: &mut A::Context, tx: Option<OneshotSender<M::Result>>) {
         if let Some(tx) = tx {
