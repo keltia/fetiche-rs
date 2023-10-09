5ccde0cf6704f5e9af607cbf34fa24ef7525166f
Ollivier Robert <roberto@keltia.net>
2023-10-09 11:17:19 +0200

    Make it async and implement Jaeger support.

--- formats/examples/parquet.rs
+++ formats/examples/parquet.rs
@@ -1,7 +1,6 @@
 //! Read some data as json and write it into a parquet file
 //!
 
-use std::fs;
 use std::fs::File;
 use std::string::ToString;
 
@@ -19,16 +18,16 @@ use tracing_tree::HierarchicalLayer;
 use fetiche_formats::Asd;
 
 #[tracing::instrument]
-fn read_data(fname: &str) -> eyre::Result<Vec<Asd>> {
+async fn read_data(fname: &str) -> eyre::Result<Vec<Asd>> {
     trace!("Read data.");
-    let str = fs::read_to_string(fname)?;
+    let str = tokio::fs::read_to_string(fname).await?;
     trace!("Decode data.");
     let data: Vec<Asd> = serde_json::from_str(&str)?;
     Ok(data)
 }
 
 #[tracing::instrument(skip(data, schema))]
-fn write_output(schema: TypePtr, data: &Vec<Asd>) -> eyre::Result<()> {
+async fn write_output(schema: TypePtr, data: &Vec<Asd>) -> eyre::Result<()> {
     // Prepare output
     //
     let file = File::create(OUTPUT)?;
@@ -51,19 +50,24 @@ fn write_output(schema: TypePtr, data: &Vec<Asd>) -> eyre::Result<()> {
 const INPUT: &str = "asd.json";
 const OUTPUT: &str = "asd.parquet";
 
-#[tracing::instrument]
-fn main() -> eyre::Result<()> {
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
     // Initialise logging early
     //
     let tree = HierarchicalLayer::new(2)
         .with_ansi(true)
-        .with_thread_names(true)
         .with_span_retrace(true)
         .with_targets(true)
         .with_verbose_entry(true)
         .with_verbose_exit(true)
         .with_higher_precision(true)
         .with_bracketed_fields(true);
+
+    // Setup Open Telemetry with Jaeger
+    //
+    let tracer =
+        opentelemetry_jaeger::new_agent_pipeline().install_batch(opentelemetry::runtime::Tokio)?;
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
 
     // Load filters from environment
     //
@@ -74,10 +78,11 @@ fn main() -> eyre::Result<()> {
     tracing_subscriber::registry()
         .with(filter)
         .with(tree)
+        .with(telemetry)
         .init();
     trace!("Logging initialised.");
 
-    let data = read_data(INPUT)?;
+    let data = read_data(INPUT).await?;
 
     info!("{} records read", data.len());
 
@@ -86,7 +91,8 @@ fn main() -> eyre::Result<()> {
     let schema = data.as_slice().schema()?;
 
     trace!("Prepare output");
-    let _ = write_output(schema, &data);
+    let _ = write_output(schema, &data).await;
 
+    opentelemetry::global::shutdown_tracer_provider();
     Ok(())
 }
