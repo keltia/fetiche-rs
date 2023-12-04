494f62cacf659ec2e1d6a09ff79945598083694d
Ollivier Robert <roberto@keltia.net>
2023-12-04 15:01:20 +0100

    Gate "flightaware" behind a feature for now.

--- formats/src/lib.rs
+++ formats/src/lib.rs
@@ -26,6 +26,7 @@ pub use asd::*;
 pub use asterix::*;
 pub use avionix::*;
 pub use drone::*;
+#[cfg(feature = "flightaware")]
 pub use flightaware::*;
 pub use opensky::*;
 pub use parquet::*;
@@ -36,6 +37,7 @@ mod asd;
 mod asterix;
 mod avionix;
 mod drone;
+#[cfg(feature = "flightaware")]
 mod flightaware;
 mod opensky;
 mod parquet;
