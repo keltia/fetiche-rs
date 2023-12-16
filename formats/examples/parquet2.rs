e075b6b695f741f86fd62d8306b30d39adc7f1f1
Ollivier Robert <roberto@keltia.net>
2023-12-17 00:19:24 +0100

    Merge parquet25 into parquet2: definitely move to 0.9.0-rc.1 API for serde_arrow.

--- formats/examples/parquet2.rs
+++ formats/examples/parquet2.rs
@@ -2,12 +2,46 @@
 //!
 //! Alternative version using `arrow2` instead of arrow/parquet:etc.
 //!
+//! Benchmarks (hyperfine): 55990279 bytes, 136373 records  -- Mac Studio M2 Pro, dev builds
+//!
+//! Uncompressed size:
+//! Zstd, default compression 3
+//! ```text
+//! Benchmark 1: cargo run --example parquet25 2023-jul-nov2
+//!   Time (mean ± σ):     10.393 s ±  0.031 s    [User: 9.965 s, System: 0.168 s]
+//!   Range (min … max):   10.361 s … 10.456 s    10 runs
+//! ```
+//! Size: 2881674
+//!
+//! Zstd, compression 8
+//! ```text
+//! Benchmark 1: cargo run --example parquet25 2023-jul-nov2
+//!   Time (mean ± σ):     11.163 s ±  0.033 s    [User: 10.726 s, System: 0.175 s]
+//!   Range (min … max):   11.131 s … 11.225 s    10 runs
+//! ```
+//! Size: 2558422
+//!
+//! Brotli, default level
+//! ```text
+//! Benchmark 1: cargo run --example parquet25 2023-jul-nov2
+//!   Time (mean ± σ):     11.197 s ±  0.196 s    [User: 10.755 s, System: 0.167 s]
+//!   Range (min … max):   11.010 s … 11.517 s    10 runs
+//! ```
+//! Size: 2943996
+//!
+//! For fun:
+//! Zstd, max compression level 22
+//! ```text
+//! Benchmark 1: cargo run --example parquet25 2023-jul-nov2
+//!   Time (mean ± σ):     35.282 s ±  0.304 s    [User: 33.202 s, System: 1.802 s]
+//!   Range (min … max):   35.044 s … 36.099 s    10 runs
+//! ```
+//! Size: 2048914
 
 use std::fs::File;
 use std::io::BufReader;
 
 use arrow2::array::Array;
-use arrow2::io::json::write::FallibleStreamingIterator;
 use arrow2::{
     chunk::Chunk,
     datatypes::Schema,
@@ -16,10 +50,8 @@ use arrow2::{
     },
 };
 use eyre::Result;
-use parquet2::compression::ZstdLevel;
-use parquet2::encoding::Encoding;
-use serde_arrow::arrow2::{serialize_into_arrays, serialize_into_fields};
-use serde_arrow::schema::TracingOptions;
+use parquet2::{compression::ZstdLevel, encoding::Encoding};
+use serde_arrow::schema::{SerdeArrowSchema, TracingOptions};
 use serde_json::Deserializer;
 use tracing::{debug, info, trace};
 use tracing_subscriber::prelude::*;
@@ -42,15 +74,18 @@ fn read_json(base: &str) -> Result<(Schema, Vec<Box<dyn Array>>)> {
     let buf = BufReader::new(File::open(&fname)?);
     let json = Deserializer::from_reader(buf).into_iter::<Asd>();
 
-    let data: Vec<Asd> = json.map(|e| e.unwrap().fix_tm().unwrap()).collect();
+    let data: Vec<_> = json
+        .map(|e| e.unwrap().fix_tm().unwrap())
+        .collect::<Vec<_>>();
 
-    let fields = serialize_into_fields(&data, topts)?;
+    let data = data.as_slice();
+    let fields = SerdeArrowSchema::from_samples(&data, topts)?.to_arrow2_fields()?;
     trace!("fields={:?}", fields);
 
     let schema = Schema::from(fields.clone());
     debug!("schema={:?}", schema);
 
-    let arrays = serialize_into_arrays(&fields, &data)?;
+    let arrays = serde_arrow::to_arrow2(&fields, &data)?;
     debug!("arrays={:?}", arrays);
 
     Ok((schema, arrays))
@@ -60,7 +95,7 @@ fn read_json(base: &str) -> Result<(Schema, Vec<Box<dyn Array>>)> {
 fn write_chunk(schema: Schema, data: Vec<Box<dyn Array>>, base: &str) -> Result<()> {
     let options = WriteOptions {
         write_statistics: true,
-        compression: CompressionOptions::Zstd(Some(ZstdLevel::default())),
+        compression: CompressionOptions::Zstd(Some(ZstdLevel::try_new(8)?)),
         version: Version::V2,
         data_pagesize_limit: None,
     };
