d5da1bc32f5d65a4010b4db36e83a89562d4ebd3
Ollivier Robert <roberto@keltia.net>
2023-12-26 00:44:04 +0100

    Powershell script to use adsb-ff to convert csv into parquet.

---
+++ scripts/convert-csv.ps1
@@ -0,0 +1,9 @@
+param([string]$Path = ".\")
+$env:RUST_LOG = 'info'
+$files = Get-ChildItem $Path
+foreach ($file in $files)
+{
+    $fn = $Path + "\" + $file.Basename
+    echo $fn
+    adsb-ff $fn
+}
