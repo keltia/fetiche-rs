55d0c70c1e88beeddc743f980cd749486496cdab
Ollivier Robert <roberto@keltia.net>
2024-02-01 01:03:26 +0100

    WIP: tying everything together.

--- process-data/src/tasks/to_planes/mod.rs
+++ process-data/src/tasks/to_planes/mod.rs
@@ -1,45 +1,53 @@
 //! Module computing the distance from a drone to the various planes around
 //!
 
-use chrono::Datelike;
+use chrono::{DateTime, Datelike, Utc};
 use duckdb::Connection;
 use eyre::{eyre, Result};
 
 use crate::cli::PlanesOpts;
-use crate::tasks::to_planes::location::load_locations;
+use crate::tasks::to_planes::location::{load_locations, Location};
 
 mod location;
 
-/// Select a list of airplanes positions we will consider for distance calculations
+/// This is the struct in which we store the context of a given day work.
 ///
-/// - 1st criteria date and time (unit is a given day)
-/// - define a bounding box around a specific site (default is 70nm) and use it as a filter
-///
-fn select_planes(dbh: &Connection, opts: &PlanesOpts) -> Result<()> {
-    let day = opts.date.day();
-    let month = opts.date.month();
-    let year = opts.date.year();
+struct Context {
+    pub name: Location,
+    pub date: DateTime<Utc>,
+}
 
-    // All flights for a given day in a table
-    //
-    // $1 = site
-    // $2 = year
-    // $3 = month
-    // $4 = start of day
-    // $5 = end of day
-    // $6 = lon of site
-    // $7 = lat of site
-    // $8 = distance in degrees (== dist(nm) /  60)   1 deg ~ 60 nm
-    //
-    let r1 = r##"
+impl Context {
+    /// Select a list of airplanes positions we will consider for distance calculations
+    ///
+    /// - 1st criteria date and time (unit is a given day)
+    /// - define a bounding box around a specific site (default is 70nm) and use it as a filter
+    ///
+    fn select_planes(&self, dbh: &Connection) -> Result<u32> {
+        let day = self.date.day();
+        let month = self.date.month();
+        let year = self.date.year();
+
+        // All flights for a given day in a table
+        //
+        // $1 = site
+        // $2 = year
+        // $3 = month
+        // $4 = start of day
+        // $5 = end of day
+        // $6 = lon of site
+        // $7 = lat of site
+        // $8 = distance in degrees (== dist(nm) /  60)   1 deg ~ 60 nm
+        //
+        let r1 = r##"
 CREATE TEMP TABLE today AS
 SELECT
-  "073.TimeRecPosition" AS time,
-  "080.AircraftAddress" AS ident,
-  "131.Longitude" AS px,
-  "131.Latitude" AS py,
-  CAST("140.GeometricAltitude" AS DOUBLE) AS pz,
-  "170.Callsign" AS callsign
+  TimeRecPosition AS time,
+  AircraftAddress AS ident,
+  Longitude AS px,
+  Latitude AS py,
+  CAST(GeometricAltitude AS DOUBLE) AS pz,
+  Callsign AS callsign
 FROM
   read_parquet('../adsb/**/*.parquet')
 WHERE
@@ -48,28 +56,39 @@ WHERE
   month=? AND
   time >= ? AND
   time <= ? AND
-    ST_DWithin(
-        ST_point(?, ?),
-        ST_Point(px, py),
-        ?
-    )
-    "##;
+  ST_DWithin(ST_point(?, ?), ST_Point(px, py), ?)
+"##;
 
-    let mut stmt = dbh.prepare(r1)?;
+        let mut stmt = dbh.prepare(r1)?;
+        let list_planes = stmt.query_map([], |row| {})?;
 
-    Ok(())
-}
+        let rc = r##"
+SELECT COUNT(*) FROM today
+"##;
+        let mut stmt = dbh.prepare(rc)?;
+        let res = stmt.query_map([], |row| {
+            let count = row.get_unwrap(0);
+            count
+        })?;
+        Ok(res)
+    }
 
-fn select_drones(dbh: &Connection, opts: &PlanesOpts) -> Result<()> {
-    // All drone points for the same day
-    //
-    // $1 = year
-    // $2 = month
-    // $3 = day
-    // $4,$5 = (lon,lat) site
-    // $6 = distance in degrees
-    //
-    let r2 = r##"
+    fn select_drones(&self, dbh: &Connection) -> Result<()> {
+        // All drone points for the same day
+        //
+        // $1 = year
+        // $2 = month
+        // $3 = day
+        // $4,$5 = (lon,lat) site
+        // $6 = distance in degrees
+        //
+        let year = self.date.year();
+        let month = self.date.month();
+        let day = self.date.day();
+
+        let location = self.name.as_str();
+
+        let r2 = r##"
 CREATE TEMP TABLE candidates AS
 SELECT *
 FROM drones
@@ -81,17 +100,19 @@ WHERE
   )
     "##;
 
-    Ok(())
-}
+        let mut stmt = dbh.prepare(r2)?;
+        let list_drones = stmt.query_map([], |row| {})?;
+        Ok(())
+    }
 
-fn find_close(dbh: &Connection, opts: &PlanesOpts) -> Result<()> {
-    // Select planes points that are in temporal and geospatial proximity +- 3 nm ~ 0.05 deg and
-    // altitude diff is less than 3 nm.
-    //
-    // $1,$2 = lon,lat of site
-    // $3 = timestamp of drone point
-    //
-    let r = r##"
+    fn find_close(&self, dbh: &Connection) -> Result<()> {
+        // Select planes points that are in temporal and geospatial proximity +- 3 nm ~ 0.05 deg and
+        // altitude diff is less than 3 nm.
+        //
+        // $1,$2 = lon,lat of site
+        // $3 = timestamp of drone point
+        //
+        let r = r##"
 CREATE TEMP TABLE close AS
 SELECT
   t.addr,
@@ -111,11 +132,12 @@ SELECT
 FROM
   today AS t,
   candidates AS c
-WHERE 
-  pt > to_timestamp(dt-2) AND 
+WHERE
+  pt > to_timestamp(dt-2) AND
   pt < to_timestamp(dt+2) AND
-  (@(pz - dz) < 5500.0)  
+  (@(pz - dz) < 5500.0)
     "##;
+    }
 }
 
 pub fn planes_calculation(dbh: &Connection, opts: PlanesOpts) -> Result<()> {
@@ -130,15 +152,26 @@ pub fn planes_calculation(dbh: &Connection, opts: PlanesOpts) -> Result<()> {
     let current = if list.get(&opts.location).is_none() {
         return Err(eyre!("Unknown location: {}", opts.location));
     } else {
-        list.get(&opts.location).unwrap()
+        list.get(&opts.location).unwrap().to_owned()
     };
 
-    let list_planes: Vec<_> = select_planes(&dbh, &opts)?;
-    if list_planes.is_empty() {
+    // Store our context
+    //
+    let ctx = Context {
+        name: current.clone(),
+        date: day,
+    };
+
+    // Create table `today` with all identified plane points with the specified range
+    //
+    let count = ctx.select_planes(&dbh)?;
+    if count == 0 {
         return Err(eyre!("No planes found around {}.", &name));
     }
 
-    let list_drones: Vec<_> = select_drones(&dbh, &opts)?;
+    // Create table `candidates` with all designated drone points
+    //
+    let count = ctx.select_drones(&dbh)?;
 
     Ok(())
 }
