727f7bcc778f0838ab3e894e70f6d1b010fbbdb5
Ollivier Robert <roberto@keltia.net>
2024-02-15 20:08:10 +0100

    Use the new versions of dist_2d/dist_3d.  Mention ST_Distance_Spheroid.

--- process-data/src/cmds/distances/to_home.rs
+++ process-data/src/cmds/distances/to_home.rs
@@ -6,17 +6,21 @@
 //!     insert both in base
 //!
 //! We also need the following macros defined:
-//! `dist_2d`, `dist_3d` and `deg_to_m`
+//! `dist_2d`, `dist_3d`
 //!
 //! cf. [DISTANCES.md](../../../docs/DISTANCES.md)
 //!
 //! NOTE: This is like 1s of runtime compared to the *several minutes* of the previous version.
 //!
 
-use crate::config::Context;
 use eyre::Result;
 
+use crate::config::Context;
+
 /// Update the given table with calculus of the distance between a drone and its operator
+///
+/// `dist_2d` has been updated to use `ST_Distance_Spheroid()`
+/// `dist_3d` has been updated to use `dist_2d`.
 ///
 #[tracing::instrument(skip(ctx))]
 pub fn home_calculation(ctx: &Context) -> Result<()> {
