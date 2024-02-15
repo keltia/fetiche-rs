abaa919888c60d1a8d2655c92abcb56bc1fb7b36
Ollivier Robert <roberto@keltia.net>
2024-02-15 17:11:32 +0100

    Reorder macro creation.

--- process-data/src/cmds/setup.rs
+++ process-data/src/cmds/setup.rs
@@ -26,16 +26,16 @@ pub struct SetupOpts {
 #[tracing::instrument(skip(dbh))]
 fn add_macros(dbh: &Connection) -> Result<()> {
     let r = r##"
-CREATE MACRO dist_2d(px, py, dx, dy) AS
-  deg_to_m(ST_Distance(ST_Point(px, py), ST_Point(dx, dy)));
-CREATE MACRO dist_3d(px, py, pz, dx, dy, dz) AS
-  sqrt(pow(dist_2d(px, py, dx, dy), 2) + pow((pz - dz), 2));
 CREATE MACRO nm_to_deg(nm) AS
   nm * 1.852 / 111111.11;
 CREATE MACRO deg_to_m(deg) AS
   deg * 111111.11;
 CREATE MACRO m_to_deg(m) AS
   m / 111111.11;
+CREATE MACRO dist_2d(px, py, dx, dy) AS
+  deg_to_m(ST_Distance(ST_Point(px, py), ST_Point(dx, dy)));
+CREATE MACRO dist_3d(px, py, pz, dx, dy, dz) AS
+  sqrt(pow(dist_2d(px, py, dx, dy), 2) + pow((pz - dz), 2));
 CREATE MACRO encounter(tm, journey, id) AS
   printf('%04d%02d%02d_%d_%d', datepart('year', CAST(tm AS DATE)), datepart('month', CAST(tm AS DATE)), datepart('day', CAST(tm AS DATE)), journey, id);
     "##;
