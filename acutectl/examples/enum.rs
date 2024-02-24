77ad29c20428c91b7dd983c33cff4ccb7178ac80
Ollivier Robert <roberto@keltia.net>
2024-02-24 15:00:54 +0100

    Finish the PoC, now to refactor at the target places.

--- acutectl/examples/enum.rs
+++ acutectl/examples/enum.rs
@@ -1,27 +1,108 @@
-use chrono::{DateTime, Utc};
+//! Proof of concept about parsing date parameters with `clap` and parameters.
+//!
+
+use chrono::{DateTime, Datelike, Days, TimeDelta, TimeZone, Utc};
 use clap::Parser;
+use eyre::bail;
+use std::ops::{Add, Sub};
 
 #[derive(Debug, Parser)]
 struct Opts {
     #[clap(subcommand)]
-    pub cmd: Try,
+    pub cmd: Cmd,
 }
 
 #[derive(Debug, Parser)]
-enum Try {
-    Single { flag: bool },
-    Multiple { begin: DateTime<Utc>, end: DateTime<Utc> },
+enum Cmd {
+    Fetch(FetchOpts),
+    Compute(CompOpts),
 }
 
-fn main() {
+#[derive(Debug, Parser)]
+struct FetchOpts {
+    #[clap(subcommand)]
+    opts: DateOpts,
+}
+
+#[derive(Clone, Debug, Parser)]
+enum DateOpts {
+    /// Basic from to
+    From { begin: String, end: String },
+    /// Specific day
+    Day { date: String },
+    /// Specific week
+    Week { num: i64 },
+    /// Shortcut for today
+    Today,
+    /// Shortcut to yesterday
+    Yesterday,
+}
+
+#[derive(Debug, Parser)]
+struct CompOpts {
+    #[clap(long)]
+    pub now: bool,
+}
+
+fn main() -> eyre::Result<()> {
     let opts: Opts = Opts::parse();
 
     match opts.cmd {
-        Try::Single { flag } => {
-            eprintln!("got single: {}", flag);
-        }
-        Try::Multiple { begin, end } => {
-            eprintln!("begin={} end={}", begin, end);
+        Cmd::Fetch(opts) => match opts.opts {
+            DateOpts::Today => {
+                eprintln!("got today true");
+                let today = Utc::now();
+                let begin = Utc
+                    .with_ymd_and_hms(today.year(), today.month(), today.day(), 0, 0, 0)
+                    .unwrap();
+                let end = begin.add(Days::new(1));
+                eprintln!("today gives from {} to {}", begin, end);
+            }
+            DateOpts::Yesterday => {
+                eprintln!("got yesterday true");
+                let today = Utc::now();
+                let yest = today.sub(Days::new(1));
+                let begin = Utc
+                    .with_ymd_and_hms(yest.year(), yest.month(), yest.day(), 0, 0, 0)
+                    .unwrap();
+                let end = Utc
+                    .with_ymd_and_hms(today.year(), today.month(), today.day(), 0, 0, 0)
+                    .unwrap();
+                eprintln!("yesterday gives from {} to {}", begin, end);
+            }
+            DateOpts::Day { date } => {
+                let begin = match dateparser::parse(&date) {
+                    Ok(date) => date,
+                    Err(_) => return bail!("Bad date."),
+                };
+                let begin = Utc
+                    .with_ymd_and_hms(begin.year(), begin.month(), begin.day(), 0, 0, 0)
+                    .unwrap();
+                let end = begin.add(Days::new(1));
+                eprintln!("this day={} gives from {} to {}", date, begin, end);
+            }
+            DateOpts::Week { num } => {
+                let week = Utc::now();
+                let begin: DateTime<Utc> =
+                    Utc.with_ymd_and_hms(week.year(), 1, 1, 0, 0, 0).unwrap();
+                let target = begin.checked_add_signed(TimeDelta::weeks(num - 1)).unwrap();
+                eprintln!("this week={} is {}", num, target);
+            }
+            DateOpts::From { begin, end } => {
+                let begin = match dateparser::parse(&begin) {
+                    Ok(date) => date,
+                    Err(_) => return bail!("Bad date."),
+                };
+                let end = match dateparser::parse(&end) {
+                    Ok(date) => date,
+                    Err(_) => return bail!("Bad date."),
+                };
+                eprintln!("begin={} end={}", begin, end);
+            }
+        },
+        Cmd::Compute(opts) => {
+            todo!()
         }
     }
+    Ok(())
 }
