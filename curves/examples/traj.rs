c1e890ed49cc875a45a30238f26582951795ea4d
Ollivier Robert <roberto@keltia.net>
2024-03-03 21:22:53 +0100

    WIP: working on  curves.

--- curves/examples/traj.rs
+++ curves/examples/traj.rs
@@ -1,1 +1,24 @@
-fn main() {}
+use csv::ReaderBuilder;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Point {
+    longitude: f64,
+    latitude: f64,
+    altitude: u32,
+}
+
+
+fn main() -> eyre::Result<()> {
+    let fname = "points.csv";
+    let mut csv = ReaderBuilder::new().has_headers(true).from_path(fname)?;
+    let rows = csv.deserialize().map(|rec| {
+        let rec: Point = rec.unwrap();
+        (rec.longitude, rec.latitude, rec.altitude as f64)
+    }).collect::<Vec<_>>();
+    dbg!(&rows);
+
+    let
+
+        Ok(())
+}
