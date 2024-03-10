4ea2812274264c9fab50ef20c4d6190d088d6ce5
Ollivier Robert <roberto@keltia.net>
2024-03-10 17:46:44 +0100

    With black_box() we don't need "d" anymore, simplify.

--- process-data/benches/distances.rs
+++ process-data/benches/distances.rs
@@ -16,9 +16,9 @@ impl Pt {
 
         let a = (d_lat / 2.0).sin() * (d_lat / 2.0).sin()
             + self.latitude.to_radians().cos()
-                * other.latitude.to_radians().cos()
-                * (d_lon / 2.0).sin()
-                * (d_lon / 2.0).sin();
+            * other.latitude.to_radians().cos()
+            * (d_lon / 2.0).sin()
+            * (d_lon / 2.0).sin();
 
         let c = 2.0 * a.sqrt().atan2((1.0 - a).sqrt());
         Pt::R * c
@@ -30,8 +30,8 @@ impl Pt {
 
         let a = (self.latitude.to_radians()).sin() * (other.latitude.to_radians()).sin()
             + (self.latitude.to_radians()).cos()
-                * (other.latitude.to_radians()).cos()
-                * d_lon.cos();
+            * (other.latitude.to_radians()).cos()
+            * d_lon.cos();
 
         let c = a.acos();
 
@@ -53,7 +53,6 @@ fn setup() -> (Pt, Pt) {
 
 fn self_haversines(c: &mut Criterion) {
     eprintln!("haversines");
-    let mut d: f64 = 1.;
     let (point1, point2) = setup();
 
     c.bench_function("self::haversines", move |b| {
@@ -61,25 +60,21 @@ fn self_haversines(c: &mut Criterion) {
             black_box(point1.haversine_distance(&point2));
         })
     });
-    let _ = d;
 }
 
 fn self_cosinuses(c: &mut Criterion) {
     eprintln!("cosinuses");
-    let mut d: f64 = 1.;
     let (point1, point2) = setup();
 
     c.bench_function("self::sincosines", |b| {
         b.iter(|| {
-            black_box(point1.spherical_law_of_cosines_distance(&point2));
+            point1.spherical_law_of_cosines_distance(black_box(&point2));
         })
     });
-    let _ = d;
 }
 
 fn geo_geodesic(c: &mut Criterion) {
     eprintln!("geodesic");
-    let mut d: f64 = 1.;
     let (point1, point2) = setup();
 
     let p1 = point!(x: point1.longitude, y: point1.latitude);
@@ -90,12 +85,10 @@ fn geo_geodesic(c: &mut Criterion) {
             black_box(p1.geodesic_distance(&p2));
         })
     });
-    let _ = d;
 }
 
 fn geo_haversines(c: &mut Criterion) {
     eprintln!("haversines");
-    let mut d: f64 = 1.;
     let (point1, point2) = setup();
 
     let p1 = point!(x: point1.longitude, y: point1.latitude);
@@ -106,12 +99,10 @@ fn geo_haversines(c: &mut Criterion) {
             black_box(p1.haversine_distance(&p2));
         })
     });
-    let _ = d;
 }
 
 fn geo_vincenty(c: &mut Criterion) {
     eprintln!("vincenty");
-    let mut d: f64 = 1.;
     let (point1, point2) = setup();
 
     let p1 = point!(x: point1.longitude, y: point1.latitude);
@@ -122,7 +113,6 @@ fn geo_vincenty(c: &mut Criterion) {
             black_box(p1.vincenty_distance(&p2).unwrap());
         })
     });
-    let _ = d;
 }
 
 criterion_group! {
