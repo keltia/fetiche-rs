f9f6df1a8dbf35649d025c21531adc1a7ca35219
Ollivier Robert <roberto@keltia.net>
2024-03-11 15:43:35 +0100

    Add option to delete after conversion. Added documentation & comments.

--- scripts/convert-csv.py
+++ scripts/convert-csv.py
@@ -21,28 +21,35 @@ import os
 from pathlib import Path
 
 
-def convert_one(fn, action):
+def convert_one(fn, action, delete):
     """
-    Move one file into the Hve tree.
+    Convert one file from .csv/.csv.gz into .parquet
 
     :param fn: filename
-    :param action: true does move the file
+    :param action: true convert the file
+    :param delete: true delete the csv file after conversion
     :return: nothing
     """
     fname = Path(fn).stem
     ext = Path(fn).suffix
 
+    # We have a gzip file.
+    #
     if ext == ".gz":
         print(f"Got a gzip file: {fn}")
         if action:
             os.system(f"gunzip {fn}")
         ext = Path(fname).suffix
 
+    # Now we should have a csv, whether it has just been uncompressed or is a plain csv file should not matter
+    #
     if ext == ".csv":
         outp = f"{fname}.parquet"
         print(fn, " -> ", outp)
         if action:
             os.system(f"bdt convert  -s -z {fn} {outp}")
+        if delete:
+            os.remove(f"{fname}{ext}")
     else:
         print(fn, "ignored")
 
@@ -54,6 +61,7 @@ parser = argparse.ArgumentParser(
     description='Uncompress and convert into parquet every csv file.')
 
 parser.add_argument('--dry-run', '-n', action='store_true', help="Do not actually move the file.")
+parser.add_argument('--delete', 'd', action='store_true', help="Remove csv after conversion.")
 parser.add_argument('files', nargs='*', help='List of files.')
 args = parser.parse_args()
 
@@ -62,7 +70,12 @@ if args.dry_run:
 else:
     action = True
 
+if args.delete:
+    delete = True
+else:
+    delete = False
+
 files = args.files
 for file in files:
     print(f"Looking at {file}")
-    convert_one(file, action)
+    convert_one(file, action, delete)
