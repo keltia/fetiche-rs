d8f80e4143861da38412372bdb153d471afd3b0d
Ollivier Robert <roberto@keltia.net>
2024-03-12 00:52:25 +0100

    Make example work.

--- common/examples/dates.rs
+++ common/examples/dates.rs
@@ -1,17 +1,28 @@
 use chrono::{DateTime, Utc};
 use eyre::Result;
+use humantime::parse_duration;
 
-async fn test_humantime() {
-    let base = "2024-03-08 12:34:56";
+async fn test_humantime() -> Result<()> {
+    let base = "2024-03-08 00:00:00";
 
-    let curr = humantime::parse_rfc3339_weak(base).unwrap();
+    let curr = humantime::parse_rfc3339_weak(base)?;
+    eprintln!("1={curr:?}");
+
+    let added = parse_duration("3h 5min")?;
+    let curr = curr + added;
+    eprintln!("2={curr:?}");
+
     let curr: DateTime<Utc> = curr.into();
-    dbg!(curr);
+    eprintln!("3={curr}");
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    test_humantime().await;
+    match test_humantime().await {
+        Ok(()) => (),
+        Err(e) => eprintln!("error={}", e.to_string()),
+    }
 
     Ok(())
 }
