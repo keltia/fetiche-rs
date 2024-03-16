6f4b5ff75c36f8330a02ff293aab406e41e1000c
Ollivier Robert <roberto@keltia.net>
2024-03-16 17:07:55 +0100

    Add NL.

--- process-data/src/cmds/stats.rs
+++ process-data/src/cmds/stats.rs
@@ -106,7 +106,7 @@ impl PlanesStats {
 impl Display for PlanesStats {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let str = format!("Day {:?}:\n{} drones in potential airprox with {} planes, {} found within {}m in a {} nm radius.\n\
-        Time spent: {} ms",
+        Time spent: {} ms\n",
                           self.day, self.drones, self.planes, self.encounters, self.proximity, self.distance, self.time);
         write!(f, "{}", str)
     }
