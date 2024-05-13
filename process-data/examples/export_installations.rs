e709b4519ce1c399620415a79de76b434a5ca55c
Ollivier Robert <roberto@keltia.net>
2024-05-13 16:43:14 +0200

    Export CSV.

---
+++ process-data/examples/export_installations.rs
@@ -0,0 +1,42 @@
+use clickhouse::Client;
+use eyre::Result;
+use tokio::fs;
+
+const URL: &str = "http://127.0.0.1:8123";
+const DB: &str = "acute";
+const USER: &str = "default";
+const FNAME: &str = "/tmp/installations.csv";
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+    let client = Client::default()
+        .with_url(URL)
+        .with_user(USER)
+        .with_database(DB)
+        .with_option("wait_end_of_query", "1");
+
+    let val = client.clone()
+        .query("SELECT geoDistance(2.319671,48.573174,2.303015, 48.566757) AS dist")
+        .fetch_one::<f64>()
+        .await?;
+
+    eprintln!("val={val}");
+
+    let r = r##"
+  SELECT * FROM acute.installations
+  INTO OUTFILE ? TRUNCATE AND STDOUT FORMAT CSV
+          "##;
+
+    let c = client.clone().query(r)
+        .bind(FNAME)
+        .fetch_one::<usize>()
+        .await?;
+
+    // Check
+    //
+    if fs::try_exists(FNAME).await? {
+        println!("Exported {c} rows to {} ", FNAME);
+    }
+    Ok(())
+}
+
