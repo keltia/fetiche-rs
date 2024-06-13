623f1691b6aed3a3c709abd8db61846a261f1d5c
Ollivier Robert <roberto@keltia.net>
2024-06-13 11:24:19 +0200

    Example script to setup Clickhouse env. variables.

---
+++ scripts/env.sh
@@ -0,0 +1,12 @@
+# THIS IS AN EXAMPLE ONLY — DO NOT USE IT WITHOUT EDITING
+#
+# YOU MUST USE IT AS SUCH
+# ```sh
+# eval "$(cat /where/ever/etc/env.sh)"
+# ```
+#
+export CLICKHOUSE_URL=http://SITE:8123
+export CLICKHOUSE_HOST=SITE
+export CLICKHOUSE_DB=somedb
+export CLICKHOUSE_USER=default
+export CLICKHOUSE_PASSWD=bigsecret
