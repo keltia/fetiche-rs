fffd508b39b66c9e3634976bc1ec46ada0633424
Ollivier Robert <roberto@keltia.net>
2024-07-23 17:18:37 +0200

    Use real data structures.

--- process-data/examples/insert.rs
+++ process-data/examples/insert.rs
@@ -2,10 +2,12 @@ use clickhouse::{Client, Row};
 use eyre::Result;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Row)]
-struct Item {
-    id: u32,
-    bar: String,
+#[derive(Clone, Debug, Serialize, Deserialize, Row)]
+struct Tc {
+    en_id: String,
+    journey: u32,
+    drone_id: String,
+    callsign: String,
 }
 
 #[tokio::main]
@@ -22,26 +24,26 @@ async fn main() -> Result<()> {
         .with_password(&pass);
 
     let ddl = r##"
-    CREATE TABLE IF NOT EXISTS foobar (
-         id        UInt32,
-         bar       Nullable(VARCHAR),
-    ) ENGINE = Memory
+CREATE TABLE IF NOT EXISTS new_ids (
+    drone_id VARCHAR,
+    callsign VARCHAR,
+    journey INT,
+    en_id VARCHAR,
+) ENGINE = Memory
     "##;
 
-    let mut data = Vec::<Item>::new();
-    data.push(Item { id: 0, bar: "h".into() });
-    data.push(Item { id: 1, bar: "g".into() });
-    data.push(Item { id: 2, bar: "f".into() });
-    data.push(Item { id: 3, bar: "e".into() });
-    data.push(Item { id: 4, bar: "d".into() });
-    data.push(Item { id: 5, bar: "c".into() });
-    data.push(Item { id: 6, bar: "b".into() });
-    data.push(Item { id: 7, bar: "a".into() });
-    data.push(Item { id: 8, bar: "".into() });
+    let mut data = Vec::<Tc>::new();
+    data.push(Tc { en_id: "BRU-20231128-37913-0".into(), journey: 37913, drone_id: "687CKAU0011JYP".into(), callsign: "BEL8DK".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-1".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "BIRD380".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-2".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "RYR8XL".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-3".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "SKEY420".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-4".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "BEL5WG".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-5".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "SKEY611".into() });
+    data.push(Tc { en_id: "BRU-20231128-37907-6".into(), journey: 37907, drone_id: "F4XF82376006N14Q".into(), callsign: "AEE2BR".into() });
 
     let _ = dbh.query(ddl).execute().await?;
 
-    let mut batch = dbh.insert("foobar")?;
+    let mut batch = dbh.insert("new_ids")?;
     for item in data.iter() {
         let _ = batch.write(item).await?;
     }
