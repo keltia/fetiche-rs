aa295bed7c14c2879915813ad7807c0e13f3397a
Ollivier Robert <roberto@keltia.net>
2024-08-17 23:21:46 +0200

    Let's try directories.

---
+++ sources/examples/dirs.rs
@@ -0,0 +1,15 @@
+use directories::ProjectDirs;
+use tracing::error;
+
+fn main() {
+    let p = ProjectDirs::from("", "", "drone-utils");
+    match p {
+        Some(p) => println!("path = {p:?}"),
+        None => panic!("grr"),
+    }
+
+    let homedir = std::env::var("LOCALAPPDATA")
+        .map_err(|e| error!("No LOCALAPPDATA variable defined, can not continue"))
+        .unwrap();
+    println!("home={homedir}");
+}
