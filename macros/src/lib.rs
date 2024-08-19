83018c88f19438816a69dc19d3613d9ae879f886
Ollivier Robert <roberto@keltia.net>
2024-08-19 17:10:38 +0200

    Add new "into_configfile" attribute macro.
    1. add two new fields: version and filename in a struct
    2. implement Versioned for struct

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,3 +1,5 @@
+use darling::ast::NestedMeta;
+use darling::FromMeta;
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::parse::Parser;
@@ -81,6 +83,102 @@ pub fn add_version(args: TokenStream, input: TokenStream) -> TokenStream {
                             version: #args,
                             ..Default::default()
                         }
+                    }
+                }
+            }
+        }
+        _ => panic!("#[add_version)] is only for struct with named fields"),
+    };
+    output.into()
+}
+
+#[derive(Debug, FromMeta)]
+struct ConfigArgs {
+    version: Option<usize>,
+    filename: Option<String>,
+}
+
+impl Default for ConfigArgs {
+    fn default() -> Self {
+        Self {
+            version: Some(1),
+            filename: Some(String::from("config.hcl")),
+        }
+    }
+}
+
+/// Add a `version(usize)` with to any given `struct` and implement the `Versioned`trait for it
+///
+#[proc_macro_attribute]
+pub fn into_configfile(args: TokenStream, input: TokenStream) -> TokenStream {
+    // Parse attributes
+    //
+    let attr_args = NestedMeta::parse_meta_list(args.into()).unwrap_or_else(|_| vec![]);
+
+    // Parse struct
+    //
+    let mut input = parse_macro_input!(input as DeriveInput);
+    let ident = &input.ident;
+
+    // Now transform the attributes into the actual data
+    //
+    let args = if attr_args.is_empty() {
+        ConfigArgs::default()
+    } else {
+        ConfigArgs::from_list(&attr_args).unwrap()
+    };
+    dbg!(&args);
+
+    let version_value = args.version.unwrap();
+    let filename = args.filename.unwrap();
+
+    // Prepare our substitutions
+    //
+    let version_ident = Ident::new("version", ident.span());
+    let version_type = quote! { usize };
+    let filename_ident = Ident::new("filename", ident.span());
+    let filename_type = quote! { String };
+
+    // Generate output
+    //
+    let output = match input.data {
+        Data::Struct(ref mut data_struct) => {
+            match &mut data_struct.fields {
+                Fields::Named(fields) => {
+                    fields.named.push(
+                        syn::Field::parse_named
+                            .parse2(quote! { #version_ident: #version_type })
+                            .unwrap(),
+                    );
+                    fields.named.push(
+                        syn::Field::parse_named
+                            .parse2(quote! { #filename_ident: #filename_type })
+                            .unwrap(),
+                    );
+                }
+                _ => unimplemented!(),
+            }
+
+            quote! {
+                #input
+
+                impl Versioned for #ident {
+                    fn version(&self) -> #version_type {
+                        self.version
+                    }
+                }
+
+                impl #ident {
+                    pub fn new() -> Self {
+                        Self {
+                            version: #version_value,
+                            filename: String::from(#filename),
+                            ..Default::default()
+                        }
+                    }
+
+                    pub fn filename(&self) -> #filename_type {
+                        self.filename.clone()
                     }
                 }
             }
