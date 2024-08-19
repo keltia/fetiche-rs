4dacf23541958d5b5423ead45feabfdda62a5caf
Ollivier Robert <roberto@keltia.net>
2024-08-19 17:13:21 +0200

    Demonstrate both "add_version" and "into_configfile".

--- common/examples/version.rs
+++ common/examples/version.rs
@@ -1,15 +1,46 @@
 use fetiche_common::Versioned;
-use fetiche_macros::add_version;
+use fetiche_macros::{add_version, into_configfile};
+use serde::Deserialize;
 
 #[add_version(2)]
 #[derive(Debug, Default)]
-pub struct Foo {
+struct Foo {
     pub name: String,
 }
 
+// Specify version and filename.
+//
+#[into_configfile(version = 3, filename = "bar.hcl")]
+#[derive(Debug, Default, Deserialize)]
+struct Bar {
+    pub value: u32,
+}
+
+// Use default defined in the macro.
+//
+#[into_configfile]
+#[derive(Debug, Default, Deserialize)]
+struct Baz {
+    pub data: u32,
+}
+
+
 fn main() {
     let foo = Foo::new();
+    let bar = Bar::new();
+    let toto = Baz::new();
 
+    dbg!(&foo);
     assert_eq!(2, foo.version());
     println!("struct Foo version is {}", foo.version());
+
+    dbg!(&bar);
+    assert_eq!(3, bar.version());
+    assert_eq!("bar.hcl", bar.filename());
+    println!("struct Bar version is {} from {}", bar.version(), bar.filename());
+
+    dbg!(&toto);
+    assert_eq!(1, toto.version());
+    assert_eq!("config.hcl", toto.filename());
+    println!("struct Baz version is {} from {}", toto.version(), toto.filename());
 }
