f286801846e485522281226cf9332a73cb060880
Ollivier Robert <roberto@keltia.net>
2024-08-20 13:31:18 +0200

    Sync with new ConfigFile structure.

--- common/examples/cfg.rs
+++ common/examples/cfg.rs
@@ -4,28 +4,26 @@
 use eyre::Result;
 use serde::Deserialize;
 
-use fetiche_common::{init_logging, ConfigEngine, Versioned};
+use fetiche_common::{init_logging, ConfigFile, IntoConfig, Versioned};
+use fetiche_macros::into_configfile;
 
 const CVERSION: usize = 1;
 
-#[derive(Clone, Debug, Deserialize)]
+#[into_configfile]
+#[derive(Clone, Default, Debug, Deserialize)]
 struct Foo {
-    version: usize,
-    pub name: String,
-}
-
-impl Versioned for Foo {
-    fn version(&self) -> usize {
-        self.version
-    }
+    pub _name: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     init_logging("cfg", false)?;
 
-    let b: Foo = ConfigEngine::load(Some("examples/local.hcl"))?;
+    let base = directories::BaseDirs::new().unwrap();
+    dbg!(&base);
+    let b = ConfigFile::<Foo>::load(Some("examples/local.hcl"))?;
     dbg!(&b);
+    let b = b.inner().unwrap();
 
     assert_eq!(CVERSION, b.version());
     Ok(())
