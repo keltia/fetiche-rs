14b68200566ecc5bc80e516ca34f34e110fe5ae2
Ollivier Robert <roberto@keltia.net>
2024-08-20 16:36:33 +0200

    Add more stuff to display.

--- common/examples/cfg.rs
+++ common/examples/cfg.rs
@@ -3,6 +3,7 @@
 
 use eyre::Result;
 use serde::Deserialize;
+use std::path::Path;
 
 use fetiche_common::{init_logging, ConfigFile, IntoConfig, Versioned};
 use fetiche_macros::into_configfile;
@@ -12,7 +13,7 @@ const CVERSION: usize = 1;
 #[into_configfile]
 #[derive(Clone, Default, Debug, Deserialize)]
 struct Foo {
-    pub _name: String,
+    pub name: String,
 }
 
 #[tokio::main]
@@ -21,10 +22,27 @@ async fn main() -> Result<()> {
 
     let base = directories::BaseDirs::new().unwrap();
     dbg!(&base);
-    let b = ConfigFile::<Foo>::load(Some("examples/local.hcl"))?;
-    dbg!(&b);
-    let b = b.inner();
-
+    let a = ConfigFile::<Foo>::load(Some("examples/local.hcl"))?;
+    dbg!(&a);
+    let b = a.inner();
+    println!("{:?}", a.list());
+    
     assert_eq!(CVERSION, b.version());
+
+    // wrong type for default "config.hcl"
+    //
+    let c = ConfigFile::<Foo>::load(None);
+    assert!(c.is_err());
+
+    // no "local.hcl" in basedir
+    //
+    let c = ConfigFile::<Foo>::load(Some("local.hcl"));
+    assert!(c.is_err());
+
+    assert!(Path::new("foo").is_relative());
+    assert!(Path::new("../foo").is_relative());
+    assert!(Path::new("./foo").is_relative());
+
+    assert!(Path::new("/foo").is_absolute());
     Ok(())
 }
