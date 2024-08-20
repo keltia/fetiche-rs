ddb953760d4e440eb6ee3fe50d9497569a208a87
Ollivier Robert <roberto@keltia.net>
2024-08-20 13:37:24 +0200

    Change ConfigEngine into ConfigFile.  Still encapsulate but include the loaded file.

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,14 +1,16 @@
-//! This is the `ConfigEngine` struct.
+//! This is the `ConfigFile` struct.
 //!
 //! This is for finding the right default locations for various configuration files for
-//! `fetiche`.  This is a a configuration file/struct neutral loading engine, storing only the
+//! `fetiche`.  This is a configuration file/struct neutral loading engine, storing only the
 //! base directory and with `load()` read the proper file or the default one.
 //!
+//! This encapsulates the configuration file, available with `.inner()` or `.inner_mut()`.
+//!
 
-use crate::{makepath, IntoConfig, Versioned};
+use crate::{makepath, IntoConfig};
 
 use directories::BaseDirs;
-use eyre::Result;
+use eyre::{eyre, Result};
 use serde::de::DeserializeOwned;
 use std::fmt::Debug;
 use std::marker::PhantomData;
@@ -26,14 +28,15 @@ const TAG: &str = "drone-utils";
 /// credentials for the various sources.
 ///
 #[derive(Debug)]
-pub struct ConfigEngine<T: Debug + DeserializeOwned + IntoConfig> {
-    /// Version in the file MUST match `CVERSION`
+pub struct ConfigFile<T: Debug + DeserializeOwned + IntoConfig> {
+    /// Tag is the project name.
     tag: String,
+    /// This is the base directory for all files.
     basedir: PathBuf,
-    _a: PhantomData<T>,
+    inner: Option<T>,
 }
 
-impl<T> ConfigEngine<T>
+impl<T> ConfigFile<T>
 where
     T: Debug + DeserializeOwned + IntoConfig,
 {
@@ -43,7 +46,12 @@ where
 
         let basedir: PathBuf = match base {
             Some(base) => {
-                let base = base.config_local_dir().to_string_lossy().to_string();
+                #[cfg(unix)]
+                let base = base.home_dir().join(".config").to_string_lossy().to_string();
+
+                #[cfg(windows)]
+                let base = base.data_local_dir().to_string_lossy().to_string();
+
                 debug!("base = {base}");
                 let base: PathBuf = makepath!(base, tag);
                 base
@@ -70,10 +78,10 @@ where
                 base
             }
         };
-        ConfigEngine {
+        ConfigFile {
             tag: String::from(tag),
             basedir,
-            _a: PhantomData,
+            inner: None,
         }
     }
 
@@ -93,17 +101,34 @@ where
         cfg
     }
 
+    /// Load the file and return a struct T in the right format.
+    ///
+    /// Use the following search path:
+    /// - default basedir (base on $HOME or $LOCALAPPDATA)
+    /// - file specified on CLI
+    ///
     #[tracing::instrument]
-    pub fn load(fname: Option<&str>) -> Result<T> {
-        trace!("loading config");
+    pub fn load(fname: Option<&str>) -> Result<ConfigFile<T>> {
+        // Create context
+        //
+        // FIXME: TAG is hardcoded.
+        //
+        let mut cfg = ConfigFile::<T>::new(TAG);
 
-        let cfg = ConfigEngine::<T>::new(TAG);
-
-        trace!("Loading {fname:?}");
         let fname = match fname {
             Some(fname) => PathBuf::from(fname),
             None => cfg.default_file(),
         };
+
+        // Use a full path
+        //
+        let fname = if fname.exists() {
+            PathBuf::from(fname).canonicalize()?
+        } else {
+            return Err(eyre!("Unknown config file {:?} and no default in {:?}", fname, cfg.default_file()));
+        };
+
+        trace!("Loading config file {fname:?} from {:?}", cfg.config_path());
 
         let data = fs::read_to_string(fname)?;
         debug!("string data = {data}");
@@ -111,13 +136,27 @@ where
         let data: T = hcl::from_str(&data)?;
         debug!("struct data = {data:?}");
 
-        Ok(data)
+        cfg.inner = Some(data);
+        Ok(cfg)
+    }
+
+    /// Return the inner configuration file
+    ///
+    pub fn inner(&self) -> Option<&T> {
+        self.inner.as_ref()
+    }
+
+    /// Return the inner configuration file as putable
+    ///
+    pub fn inner_mut(&mut self) -> Option<&mut T> {
+        self.inner.as_mut()
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{IntoConfig, Versioned};
     use fetiche_macros::into_configfile;
     use serde::{Deserialize, Serialize};
     use std::collections::BTreeMap;
@@ -146,8 +185,8 @@ mod tests {
     /// credentials for the various sources.
     ///
     #[into_configfile]
-    #[derive(Debug, Deserialize)]
-    struct ConfigFile {
+    #[derive(Debug, Default, Deserialize)]
+    struct Bar {
         /// Each site credentials
         pub site: BTreeMap<String, Auth>,
     }
@@ -158,23 +197,25 @@ mod tests {
     fn test_config_engine_load_default() -> Result<()> {
         // Explicitly load default
         //
-        let cfg: ConfigFile = ConfigEngine::load(None)?;
+        let cfg = ConfigFile::<Bar>::load(None)?;
         dbg!(&cfg);
-        assert_eq!(CVERSION, cfg.version());
+        let inner = cfg.inner().unwrap();
+        assert_eq!(CVERSION, inner.version());
         Ok(())
     }
 
     #[into_configfile]
-    #[derive(Clone, Debug, Deserialize)]
+    #[derive(Clone, Debug, Default, Deserialize)]
     struct Foo {
         pub name: String,
     }
 
     #[test]
     fn test_config_engine_load_file() -> Result<()> {
-        let cfg: Foo = ConfigEngine::load(Some("examples/local.hcl"))?;
+        let cfg = ConfigFile::<Foo>::load(Some("examples/local.hcl"))?;
         dbg!(&cfg);
-        assert_eq!(CVERSION, cfg.version());
+        let inner = cfg.inner().unwrap();
+        assert_eq!(CVERSION, inner.version());
         Ok(())
     }
 }
