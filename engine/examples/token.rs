f83309ffdc22e980e2cdc4cbe754c20ef3bc6144
Ollivier Robert <roberto@keltia.net>
2024-08-23 22:52:34 +0200

    Use the right paths.

--- engine/examples/token.rs
+++ engine/examples/token.rs
@@ -1,6 +1,11 @@
 use fetiche_engine::TokenStorage;
 
 fn main() {
+    #[cfg(windows)]
+    let storage = TokenStorage::register("/Users/roberto/AppData/Local/drone-utils/tokens");
+
+    #[cfg(unix)]
     let storage = TokenStorage::register("/Users/roberto/.config/drone-utils/tokens");
+
     dbg!(&storage);
 }
