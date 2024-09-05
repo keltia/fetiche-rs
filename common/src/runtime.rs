5df08c3e19cf70b6e308809621cd8b2b07945027
Ollivier Robert <roberto@keltia.net>
2024-09-05 14:40:48 +0200

    Revisit the way we trace: add a file appender and options for tree/otlp.

--- common/src/runtime.rs
+++ common/src/runtime.rs
@@ -10,41 +10,62 @@ use tracing_subscriber::EnvFilter;
 use tracing_tree::HierarchicalLayer;
 
 #[tracing::instrument]
-pub fn init_logging(name: &'static str, use_telemetry: bool) -> Result<()> {
+pub fn init_logging(name: &'static str, use_telemetry: bool, use_tree: bool, use_file: bool) -> Result<()> {
     // Initialise logging early
     //
-    let tree = HierarchicalLayer::new(2)
-        .with_ansi(true)
-        .with_span_retrace(true)
-        .with_span_modes(true)
-        .with_targets(true)
-        .with_verbose_entry(true)
-        .with_verbose_exit(true)
-        .with_bracketed_fields(true);
+    // Load filters from environment
+    //
+    let filter = EnvFilter::from_default_env();
 
-    let exporter = opentelemetry_otlp::new_exporter().tonic();
-    let provider = opentelemetry_otlp::new_pipeline()
-        .tracing()
-        .with_exporter(exporter)
-        .install_batch(opentelemetry_sdk::runtime::Tokio)?;
-    let tracer = provider.tracer(name);
-    let telemetry = if use_telemetry {
+    // Do we want hierarchical output?
+    //
+    let tree = if use_tree {
+        Some(HierarchicalLayer::new(2)
+            .with_ansi(true)
+            .with_span_retrace(true)
+            .with_span_modes(true)
+            .with_targets(true)
+            .with_verbose_entry(true)
+            .with_verbose_exit(true)
+            .with_bracketed_fields(true))
+    } else {
+        None
+    };
+
+    // Enable telemetry?
+    //
+    let otlp = if use_telemetry {
+        let exporter = opentelemetry_otlp::new_exporter().tonic();
+        let provider = opentelemetry_otlp::new_pipeline()
+            .tracing()
+            .with_exporter(exporter)
+            .install_batch(opentelemetry_sdk::runtime::Tokio)?;
+        let tracer = provider.tracer(name);
         Some(tracing_opentelemetry::layer().with_tracer(tracer))
     } else {
         None
     };
 
-    // Load filters from environment
+    // Log to file?
     //
-    let filter = EnvFilter::from_default_env();
+    let file = if use_file {
+        // Basic append-only rolling file for all traces.
+        //
+        let file_appender = tracing_appender::rolling::hourly("/tmp/acute", format!("{name}"));
+        Some(tracing_subscriber::fmt::layer().with_writer(file_appender))
+    } else {
+        None
+    };
 
-    // Combine filter & specific format
+    // Combine filters & exporters
     //
     tracing_subscriber::registry()
         .with(filter)
         .with(tree)
-        .with(telemetry)
+        .with(otlp)
+        .with(file)
         .init();
+
     Ok(())
 }
 
