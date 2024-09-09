25571a576a78cc467caf4da39c85ee2898307264
Ollivier Robert <roberto@keltia.net>
2024-09-09 15:06:10 +0200

    Fully implement export.

--- process-data/examples/export.rs
+++ process-data/examples/export.rs
@@ -1,12 +1,34 @@
-use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
-use eyre::Result;
-use fetiche_common::{close_logging, init_logging};
-use klickhouse::{Client, ClientOptions, QueryBuilder, Row};
-use tracing::trace;
+use std::fs;
 
-#[derive(Debug, Row)]
-struct Ans {
-    id: u32,
+use clap::{crate_authors, crate_description, crate_name, crate_version, Parser};
+use csv::WriterBuilder;
+use eyre::Result;
+use klickhouse::{Client, ClientOptions, DateTime, QueryBuilder, Row};
+use serde::{Deserialize, Serialize};
+
+use fetiche_common::{close_logging, init_logging};
+
+#[derive(Debug, Deserialize, Row, Serialize)]
+struct Encounter {
+    site: String,
+    en_id: String,
+    time: DateTime,
+    journey: i32,
+    drone_id: String,
+    model: String,
+    drone_lon: f32,
+    drone_lat: f32,
+    drone_alt_m: f32,
+    drone_height_m: f32,
+    prox_callsign: String,
+    prox_id: String,
+    prox_lon: f32,
+    prox_lat: f32,
+    prox_alt_m: f32,
+    distance_slant_m: i32,
+    distance_hor_m: i32,
+    distance_vert_m: i32,
+    distance_home_m: i32,
 }
 
 #[derive(Debug, Parser)]
@@ -29,7 +51,7 @@ async fn main() -> Result<()> {
 
     init_logging("export-encounters", false, true, true)?;
 
-    trace!("Create connection.");
+    eprintln!("Create connection.");
     let client = Client::connect(
         endpoint,
         ClientOptions {
@@ -41,8 +63,8 @@ async fn main() -> Result<()> {
         .await?;
 
     let fname = opts.fname.clone();
-    let r = format!(
-        r##"
+    eprintln!("Created fname: {}", fname);
+    let r = r##"
   SELECT
     en_id,
     site,
@@ -65,14 +87,27 @@ async fn main() -> Result<()> {
     distance_slant_m,
   FROM airplane_prox
   ORDER BY time
-  INTO OUTFILE '{}' FORMAT CSVWithNames
-        "##,
-        fname
-    );
-    trace!("q={r}");
+        "##;
 
     let q = QueryBuilder::new(&r);
-    let _ = client.execute(q).await?;
+    let res = client.query_collect::<Encounter>(q).await?;
+
+    // Prepare the writer
+    //
+    let mut wtr = WriterBuilder::new()
+        .has_headers(true)
+        .from_writer(vec![]);
+
+    // Insert data
+    //
+    res.iter().for_each(|rec| {
+        wtr.serialize(rec).unwrap();
+    });
+
+    // Output final csv
+    //
+    let data = String::from_utf8(wtr.into_inner()?)?;
+    fs::write(fname, data)?;
 
     close_logging();
     Ok(())
