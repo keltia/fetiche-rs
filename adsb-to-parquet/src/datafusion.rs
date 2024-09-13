981a42ba0e1be0c3929696beaf88b78aa0d30964
Ollivier Robert <roberto@keltia.net>
2024-09-13 19:11:00 +0200

    Fix for datafusion v40.

--- adsb-to-parquet/src/datafusion.rs
+++ adsb-to-parquet/src/datafusion.rs
@@ -3,9 +3,8 @@
 //! This is way faster than `arrow2` (see benches/csv-to-parquet.rs) for larger datasets
 //!
 
+use datafusion::config::TableParquetOptions;
 use datafusion::dataframe::DataFrameWriteOptions;
-use datafusion::parquet::basic::{Compression, Encoding, ZstdLevel};
-use datafusion::parquet::file::properties::{EnabledStatistics, WriterProperties};
 use datafusion::prelude::*;
 use eyre::Result;
 
@@ -23,25 +22,30 @@ pub async fn parquet_through_df(fname: &str, output: &str, opts: Options) -> Res
     // Setup datafusion for csv files
     //
     let ctx = SessionContext::new();
-    let copts = CsvReadOptions::new().delimiter(delim).has_header(header);
 
     // Do the reading
     //
-    let df = ctx.read_csv(fname, copts).await?;
+    let df = ctx
+        .read_csv(
+            fname,
+            CsvReadOptions::default()
+                .delimiter(delim)
+                .has_header(header),
+        )
+        .await?;
 
-    // Setup our options, both for dataframe and file
-    //
-    let dopts = DataFrameWriteOptions::default().with_single_file_output(true);
-    let props = WriterProperties::builder()
-        .set_created_by("bench_df".to_string())
-        .set_encoding(Encoding::PLAIN)
-        .set_statistics_enabled(EnabledStatistics::Page)
-        .set_compression(Compression::ZSTD(ZstdLevel::try_new(8)?))
-        .build();
+    let fname = "../data/test-df.parquet";
 
-    // Do the writing
-    //
-    let _ = df.write_parquet(output, dopts, Some(props)).await?;
+    let dfopts = DataFrameWriteOptions::default().with_single_file_output(true);
+
+    let mut options = TableParquetOptions::default();
+    options.global.created_by = "bench_polars".to_string();
+    options.global.writer_version = "2.0".to_string();
+    options.global.encoding = Some("plain".to_string());
+    options.global.statistics_enabled = Some("page".to_string());
+    options.global.compression = Some("zstd(8)".to_string());
+
+    let _ = df.write_parquet(fname, dfopts, Some(options)).await?;
 
     Ok(())
 }
