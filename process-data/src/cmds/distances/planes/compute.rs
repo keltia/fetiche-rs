18e25f2142f423358b5687e57357c8f1850043fb
Ollivier Robert <roberto@keltia.net>
2024-09-16 22:54:24 +0200

    Change dbg into debug.

--- process-data/src/cmds/distances/planes/compute.rs
+++ process-data/src/cmds/distances/planes/compute.rs
@@ -582,7 +582,7 @@ impl Calculate for PlaneDistance {
 
         self.cleanup_temp_tables(dbh).await?;
 
-        dbg!(timings);
+        debug!("timings={timings}");
 
         Ok(Stats::Planes(stats.clone()))
     }
