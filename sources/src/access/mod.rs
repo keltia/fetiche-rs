bd157b636ab7e7379044eb00fd20731087823bc8
Ollivier Robert <roberto@keltia.net>
2024-10-10 14:07:17 +0200

    Reconnect avionix.

--- sources/src/access/mod.rs
+++ sources/src/access/mod.rs
@@ -1,13 +1,14 @@
 pub use aeroscope::*;
 pub use asd::*;
-//pub use avionix::*;
+pub use avionix::*;
 pub use flightaware::*;
 pub use opensky::*;
 pub use safesky::*;
 
 mod aeroscope;
 mod asd;
-//mod avionix;
+mod avionix;
+mod error;
 mod flightaware;
 mod opensky;
 mod safesky;
