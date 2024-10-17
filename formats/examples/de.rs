9ec6024ccfcb65889ccb0347c4f10fa4bc8eba1d
Ollivier Robert <roberto@keltia.net>
2024-10-17 14:27:16 +0200

    Add example of using serde_as to handle both u32/f64 for a final u32.

--- formats/examples/de.rs
+++ formats/examples/de.rs
@@ -1,6 +1,7 @@
 use eyre::Result;
 use serde::{Deserialize, Serialize};
 use serde_json::from_str;
+use serde_with::{serde_as, serde_conv};
 use strum::EnumString;
 
 #[derive(Clone, Debug, Deserialize, EnumString, strum::Display, Serialize)]
@@ -13,6 +14,22 @@ pub enum Type {
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Foo {
     pub tm: Type,
+}
+
+serde_conv!(
+    FloatAsInt,
+    u32,
+    |x: &u32| *x as f64,
+    |value: f64| -> Result<_, std::convert::Infallible> {
+        Ok(value as u32)
+    }
+);
+
+#[serde_as]
+#[derive(Debug, Deserialize)]
+struct Bar {
+    #[serde_as(as = "FloatAsInt")]
+    pub trk: u32,
 }
 
 fn main() -> Result<()> {
@@ -28,7 +45,15 @@ fn main() -> Result<()> {
     let str = r##"{"tm": "1687363200"}"##;
 
     let a: Foo = from_str(str)?;
-
     dbg!(a);
+
+    let str = r##"{"trk": 42.3765}"##;
+    let b: Bar = from_str(str)?;
+    dbg!(&b);
+
+    let str = r##"{"trk": 666}"##;
+    let c: Bar = from_str(str)?;
+    dbg!(&c);
+
     Ok(())
 }
