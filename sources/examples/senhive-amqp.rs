a70b8cd861b9f17e3c6142cef6d9a9408e717ac7
Ollivier Robert <roberto@keltia.net>
2024-11-10 19:05:45 +0100

    Real life example with real queues.

--- sources/examples/senhive-amqp.rs
+++ sources/examples/senhive-amqp.rs
@@ -1,9 +1,13 @@
+//! Connect to a Thales Senhive antenna and fetch messages through Lapin as AMQP client.
+//!
+
+use eyre::Result;
 use futures_util::stream::StreamExt;
 use lapin::{options::*, types::FieldTable, Connection, ConnectionProperties};
 use std::env;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let url = env::var("LAPIN_URL").expect("LAPIN_URL must be set");
 
     // Connect to the AMQP server
@@ -11,44 +15,76 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Connected to RabbitMQ");
 
     // Create a channel
-    let channel = conn.create_channel().await?;
-    println!("Created a channel");
+    let data_ch = conn.create_channel().await?;
+    println!("Created data channel");
 
-    // Declare a queue (if it doesn't already exist)
-    let queue = channel
-        .queue_declare(
-            "drone_feed", // Queue name
-            QueueDeclareOptions {
-                durable: true, // Persist the queue across broker restarts
-                ..Default::default()
-            },
-            FieldTable::default(), // Extra parameters
-        )
-        .await?;
-    println!("Declared queue: {:?}", queue.name());
+    // Create a channel
+    let alert_ch = conn.create_channel().await?;
+    println!("Created data channel");
+
+    // Create a channel
+    let state_ch = conn.create_channel().await?;
+    println!("Created data channel");
 
     // Consume messages from the queue
-    let mut consumer = channel
+    let mut data = data_ch
         .basic_consume(
-            "drone_feed", // Queue name
+            "fused_data", // Queue name
             "drone_tag",  // Consumer tag
             BasicConsumeOptions::default(),
             FieldTable::default(),
         )
         .await?;
+
+    // Consume messages from the queue
+    let mut alert = alert_ch
+        .basic_consume(
+            "system_alert", // Queue name
+            "drone_tag",    // Consumer tag
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
+    // Consume messages from the queue
+    let mut state = state_ch
+        .basic_consume(
+            "system_state", // Queue name
+            "drone_tag",    // Consumer tag
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await?;
+
     println!("Waiting for messages...");
 
     // Process each message
-    while let Some(delivery) = consumer.next().await {
-        let delivery = delivery?;
-        println!(
-            "Received message: {:?}",
-            std::str::from_utf8(&delivery.data).unwrap()
-        );
+    loop {
+        tokio::select! {
+            Some(data) = data.next() => {
+                let delivery = data?;
+                println!(
+                    "Received data message: {:?}",
+                    std::str::from_utf8(&delivery.data).unwrap()
+                );
+            }
+            Some(alert) = alert.next() => {
+                let delivery = alert?;
+                println!(
+                    "Received alert message: {:?}",
+                    std::str::from_utf8(&delivery.data).unwrap()
+                );
 
-        // Acknowledge the message
-        delivery.ack(BasicAckOptions::default()).await?;
+            }
+            Some(state) = state.next() => {
+                let delivery = state?;
+                println!(
+                    "Received state message: {:?}",
+                    std::str::from_utf8(&delivery.data).unwrap()
+                );
+
+            }
+        }
     }
-
     Ok(())
 }
