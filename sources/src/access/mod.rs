20681756f2d43b3498720c0549c3ff870e88781b
Ollivier Robert <roberto@keltia.net>
2024-11-10 21:23:44 +0100

    Manage features.

--- sources/src/access/mod.rs
+++ sources/src/access/mod.rs
@@ -2,21 +2,36 @@ use std::fmt::{Display, Formatter};
 
 use serde::Serialize;
 
+#[cfg(feature = "aeroscope")]
 pub use aeroscope::*;
+#[cfg(feature = "asd")]
 pub use asd::*;
+#[cfg(feature = "avionix")]
 pub use avionix::*;
+#[cfg(feature = "flightaware")]
 pub use flightaware::*;
+#[cfg(feature = "opensky")]
 pub use opensky::*;
+#[cfg(feature = "safesky")]
 pub use safesky::*;
+#[cfg(feature = "senhive")]
 pub use senhive::*;
 
-mod aeroscope;
-mod asd;
-mod avionix;
 mod error;
+
+#[cfg(feature = "aeroscope")]
+mod aeroscope;
+#[cfg(feature = "asd")]
+mod asd;
+#[cfg(feature = "avionix")]
+mod avionix;
+#[cfg(feature = "flightaware")]
 mod flightaware;
+#[cfg(feature = "opensky")]
 mod opensky;
+#[cfg(feature = "safesky")]
 mod safesky;
+#[cfg(feature = "senhive")]
 mod senhive;
 
 /// Statistics gathering struct, should be generic enough for most sources
