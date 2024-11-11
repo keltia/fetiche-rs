7b4df65c6d324732c789a1f919a1f7781ac224a0
Ollivier Robert <roberto@keltia.net>
2024-11-11 16:49:49 +0100

    Add definition for the system_alert topic.

--- formats/src/senhive/alert.rs
+++ formats/src/senhive/alert.rs
@@ -1,3 +1,24 @@
 //! This is the module for data types for the `system_alert` / `dl_system_alert` queues
 //!
 
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use strum::EnumString;
+
+#[derive(Debug, Deserialize, strum::Display, EnumString, strum::VariantNames, Serialize)]
+#[strum(serialize_all = "UPPERCASE")]
+pub enum Severity {
+    Info,
+    Warning,
+    Error,
+    Critical,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct AlertData {
+    pub version: Option<String>,
+    pub title: String,
+    pub timestamp: DateTime<Utc>,
+    pub severity: Severity,
+    pub details: String,
+}
