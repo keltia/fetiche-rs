d2389541e0207dc324b5530477c1260bf125ea99
Ollivier Robert <roberto@keltia.net>
2024-11-11 17:05:59 +0100

    Add enums, fix some optional/not.

--- formats/src/senhive/state.rs
+++ formats/src/senhive/state.rs
@@ -1,10 +1,10 @@
 //! This is the module for data types for the `system_state` / `dl_system_state` queues
 //!
 
+use crate::senhive::Coordinates;
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-
-use crate::senhive::Coordinates;
+use strum::EnumString;
 
 // ----- queue: `system_state`
 
@@ -19,7 +19,29 @@ pub struct Service {
 pub struct Altitude {
     pub agl: Option<f64>,
     pub amsl: Option<f64>,
-    pub geodetic: f64,
+    pub geodetic: Option<f64>,
+}
+
+#[derive(Debug, Deserialize, strum::Display, EnumString, strum::VariantNames, Serialize)]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+pub enum SensorType {
+    Senid,
+    SenidPlus,
+    Seniris,
+    Radar,
+    Rf,
+    Audio,
+    Camera,
+    Unknown,
+}
+
+#[derive(Debug, Deserialize, strum::Display, EnumString, strum::VariantNames, Serialize)]
+#[strum(serialize_all = "UPPERCASE")]
+pub enum State {
+    Unknown,
+    Error,
+    Warning,
+    Ok,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -27,14 +49,15 @@ pub struct Sensor {
     pub serial: String,
     pub name: String,
     #[serde(rename = "type")]
-    pub r#type: String,
-    pub coordinates: Coordinates,
+    pub r#type: SensorType,
+    pub coordinates: Option<Coordinates>,
     pub altitude: Altitude,
+    /// Defined as a WKT POLYGON
     #[serde(rename = "estimatedCoverage")]
-    pub estimated_coverage: Option<f64>,
-    pub state: String,
+    pub estimated_coverage: Option<String>,
+    pub state: State,
     #[serde(rename = "lastOnline")]
-    pub last_online: DateTime<Utc>,
+    pub last_online: Option<DateTime<Utc>>,
     #[serde(rename = "metaData")]
     pub meta_data: Option<String>,
 }
@@ -43,7 +66,7 @@ pub struct Sensor {
 ///
 #[derive(Debug, Serialize, Deserialize)]
 pub struct StateMsg {
-    pub version: String,
+    pub version: Option<String>,
     pub name: String,
     pub timestamp: DateTime<Utc>,
     pub sensors: Vec<Sensor>,
