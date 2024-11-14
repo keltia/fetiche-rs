1e5b36da388955c7589fed902ba999745549a91b
Ollivier Robert <roberto@keltia.net>
2024-11-14 14:59:25 +0100

    clippy: Not sure why it considers we are documenting output and not the macro.

--- formats/src/avionix.rs
+++ formats/src/avionix.rs
@@ -8,50 +8,49 @@ use serde::{Deserialize, Serialize};
 use serde_with::{serde_as, serde_conv};
 use strum::EnumString;
 
-#[allow(unused_doc_comments)]
-/// Enable deserialization from either i32/f64 into the final i32.  Value is rounded up or down as
-/// needed.
-///
-/// Example:
-/// ```rust
-/// # use eyre::Result;
-/// # use serde_with::{serde_as, serde_conv};
-/// # use serde_json::from_str;
-/// # use serde::Deserialize;
-///
-/// serde_conv!(
-///     FloatAsInt,
-///     u32,
-///     |x: &u32| *x as f64,
-///     |value: f64| -> Result<_, std::convert::Infallible> {
-///         Ok((value + 0.5) as u32)
-///     }
-/// );
-///
-/// #[serde_as]
-///  #[derive(Debug, Deserialize)]
-///  struct Bar {
-///     #[serde_as(as = "FloatAsInt")]
-///     pub trk: u32,
-/// }
-///
-/// fn main() -> Result<()> {
-///    let str = r##"{"trk": 42.3765}"##;
-///    let b: Bar = from_str(str)?;
-///    assert_eq!(b.trk, 42u32);
-///
-///    let str = r##"{"trk": 42.7765}"##;
-///    let b: Bar = from_str(str)?;
-///    assert_eq!(b.trk, 43u32);
-///
-///    let str = r##"{"trk": 666}"##;
-///    let c: Bar = from_str(str)?;
-///    assert_eq!(c.trk, 666u32);
-///
-///     Ok(())
-/// }
-/// ```
-///
+// Enable deserialization from either i32/f64 into the final i32.  Value is rounded up or down as
+// needed.
+//
+// Example:
+// ```rust
+// # use eyre::Result;
+// # use serde_with::{serde_as, serde_conv};
+// # use serde_json::from_str;
+// # use serde::Deserialize;
+//
+// serde_conv!(
+//     FloatAsInt,
+//     u32,
+//     |x: &u32| *x as f64,
+//     |value: f64| -> Result<_, std::convert::Infallible> {
+//         Ok((value + 0.5) as u32)
+//     }
+// );
+//
+// #[serde_as]
+//  #[derive(Debug, Deserialize)]
+//  struct Bar {
+//     #[serde_as(as = "FloatAsInt")]
+//     pub trk: u32,
+// }
+//
+// fn main() -> Result<()> {
+//    let str = r##"{"trk": 42.3765}"##;
+//    let b: Bar = from_str(str)?;
+//    assert_eq!(b.trk, 42u32);
+//
+//    let str = r##"{"trk": 42.7765}"##;
+//    let b: Bar = from_str(str)?;
+//    assert_eq!(b.trk, 43u32);
+//
+//    let str = r##"{"trk": 666}"##;
+//    let c: Bar = from_str(str)?;
+//    assert_eq!(c.trk, 666u32);
+//
+//     Ok(())
+// }
+// ```
+//
 serde_conv!(
     FloatAsInt,
     u32,
