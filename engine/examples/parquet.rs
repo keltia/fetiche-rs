377a2f316ca41fe8fb858779cfd1f06750a8a715
Ollivier Robert <roberto@keltia.net>
2024-11-17 17:10:10 +0100

    Simplify.

--- engine/examples/parquet.rs
+++ engine/examples/parquet.rs
@@ -1,35 +1,23 @@
 //! Read some data as json and write it into a parquet file
 //!
 
-use std::fs;
 use std::fs::File;
-use std::io::BufReader;
 use std::num::NonZeroUsize;
 use std::path::Path;
 
 use eyre::Result;
-use polars::prelude::{JsonFormat, JsonLineReader, JsonReader, ParquetWriter, SerReader};
-use polars_core::prelude::*;
-use tracing::{info, trace};
-use tracing_subscriber::prelude::*;
-use tracing_subscriber::EnvFilter;
-use tracing_tree::HierarchicalLayer;
+use polars::prelude::{JsonFormat, JsonReader, ParquetWriter, SerReader};
 
-use fetiche_common::init_logging;
-
-#[tracing::instrument]
 async fn read_write_output(base: &str) -> Result<()> {
-    trace!("Read data.");
-
     let fname = Path::new(base).with_extension("json");
-    trace!("fname={:?}", fname);
+    eprintln!("Reading data from {:?}", fname);
 
     let fh = File::open(fname)?;
     let mut df = JsonReader::new(fh)
         .with_json_format(JsonFormat::JsonLines)
         .infer_schema_len(NonZeroUsize::new(3))
         .finish()?;
-    info!("{} records read", df.clone().count().await?);
+    eprintln!("{} records read", df.iter().len());
 
     // Prepare output
     //
@@ -38,17 +26,12 @@ async fn read_write_output(base: &str) -> Result<()> {
     let mut file = File::create(fname)?;
     ParquetWriter::new(&mut file).finish(&mut df)?;
 
-    info!("Done.");
+    eprintln!("Done.");
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    // Initialise logging early
-    //
-    init_logging("parquet", false, true, None)?;
-    trace!("Logging initialised.");
-
     let fname = std::env::args().nth(1).ok_or("small").unwrap();
 
     let _ = read_write_output(&fname).await?;
