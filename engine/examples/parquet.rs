180a206ba8ccd7b4415b9d8f6d9db82c3c5d37f8
Ollivier Robert <roberto@keltia.net>
2024-11-17 15:19:18 +0100

    Rewrite using polars instead of datafusion.

--- engine/examples/parquet.rs
+++ engine/examples/parquet.rs
@@ -1,41 +1,42 @@
 //! Read some data as json and write it into a parquet file
 //!
 
-use datafusion::prelude::*;
-use datafusion::{config::TableParquetOptions, dataframe::DataFrameWriteOptions};
+use std::fs;
+use std::fs::File;
+use std::io::BufReader;
+use std::num::NonZeroUsize;
+use std::path::Path;
+
 use eyre::Result;
+use polars::prelude::{JsonFormat, JsonLineReader, JsonReader, ParquetWriter, SerReader};
+use polars_core::prelude::*;
 use tracing::{info, trace};
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 use tracing_tree::HierarchicalLayer;
 
+use fetiche_common::init_logging;
+
 #[tracing::instrument]
 async fn read_write_output(base: &str) -> Result<()> {
     trace!("Read data.");
 
-    let fname = format!("{}.json", base);
+    let fname = Path::new(base).with_extension("json");
     trace!("fname={:?}", fname);
 
-    let ctx = SessionContext::new();
-
-    let df = ctx.read_json(&fname, NdJsonReadOptions::default()).await?;
+    let fh = File::open(fname)?;
+    let mut df = JsonReader::new(fh)
+        .with_json_format(JsonFormat::JsonLines)
+        .infer_schema_len(NonZeroUsize::new(3))
+        .finish()?;
     info!("{} records read", df.clone().count().await?);
 
     // Prepare output
     //
-    let fname = format!("{}.parquet", base);
+    let fname = Path::new(base).with_extension("parquet");
 
-    let dfopts = DataFrameWriteOptions::default().with_single_file_output(true);
-
-    let mut options = TableParquetOptions::default();
-    options.global.created_by = "acutectl/save".to_string();
-    options.global.writer_version = "2.0".to_string();
-    options.global.encoding = Some("plain".to_string());
-    options.global.statistics_enabled = Some("page".to_string());
-    options.global.compression = Some("zstd(8)".to_string());
-
-    info!("Writing in {}", fname);
-    let _ = df.write_parquet(&fname, dfopts, Some(options)).await?;
+    let mut file = File::create(fname)?;
+    ParquetWriter::new(&mut file).finish(&mut df)?;
 
     info!("Done.");
     Ok(())
@@ -45,24 +46,7 @@ async fn read_write_output(base: &str) -> Result<()> {
 async fn main() -> Result<()> {
     // Initialise logging early
     //
-    let tree = HierarchicalLayer::new(2)
-        .with_ansi(true)
-        .with_span_retrace(true)
-        .with_targets(true)
-        .with_verbose_entry(true)
-        .with_verbose_exit(true)
-        .with_bracketed_fields(true);
-
-    // Load filters from environment
-    //
-    let filter = EnvFilter::from_default_env();
-
-    // Combine filter & specific format
-    //
-    tracing_subscriber::registry()
-        .with(filter)
-        .with(tree)
-        .init();
+    init_logging("parquet", false, true, None)?;
     trace!("Logging initialised.");
 
     let fname = std::env::args().nth(1).ok_or("small").unwrap();
