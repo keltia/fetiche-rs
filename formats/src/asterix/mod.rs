79d2f8e4b2728b72a0f51c7ee184579c98289bd1
Ollivier Robert <roberto@keltia.net>
2024-11-26 14:48:58 +0100

    Move Cat21 specific function here.

--- formats/src/asterix/mod.rs
+++ formats/src/asterix/mod.rs
@@ -15,3 +15,15 @@ pub use cat21::*;
 pub const DEF_SAC: usize = 8;
 /// Default SIC
 pub const DEF_SIC: usize = 200;
+
+/// For privacy reasons, we truncate the drone ID value to something not unique
+///
+#[cfg(feature = "privacy")]
+pub fn get_drone_id(id: &str) -> String {
+    id[2..10].to_owned()
+}
+
+#[cfg(not(feature = "privacy"))]
+pub fn get_drone_id(id: &str) -> String {
+    id.to_owned()
+}
