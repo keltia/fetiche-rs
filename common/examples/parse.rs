e6f8719af9ea1528050ade5468d4d1c2c4ff74d7
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Fix example and its tests.

--- common/examples/parse.rs
+++ common/examples/parse.rs
@@ -4,20 +4,22 @@ use strum::{EnumString, VariantNames};
 
 #[derive(Clone, Debug, Deserialize, EnumString, Serialize, PartialEq, VariantNames)]
 #[serde(rename_all = "lowercase")]
-pub enum JobType {
-    Fetch,
-    Read,
-    Stream,
+pub enum Producer {
+    Fetch(String),
+    Read(String),
+    Stream(String),
 }
 
 #[derive(Clone, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum Filter {
+#[serde(rename_all = "lowercase")]
+pub enum Middle {
     Copy,
     Convert(String),
     Tee(String),
 }
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum Consumer {
     Archive(String),
     Store(String, Freq),
@@ -25,6 +27,7 @@ pub enum Consumer {
 }
 
 #[derive(Clone, Debug, Deserialize, EnumString, PartialEq, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum Freq {
     Daily,
     Hourly,
@@ -32,14 +35,12 @@ pub enum Freq {
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct JobText {
-    #[serde(rename = "type")]
-    pub jtype: JobType,
     /// Job name.
     pub name: String,
-    /// Source (aka Site name).
-    pub source: String,
+    /// Generator
+    pub producer: Producer,
     /// Optional list of filters like `Tee` or `Save`.
-    pub filters: Option<Vec<Filter>>,
+    pub filters: Option<Vec<Middle>>,
     /// Output file name.
     pub output: Consumer,
 }
@@ -47,35 +48,54 @@ pub struct JobText {
 impl JobText {
     pub fn parse(job_str: &str) -> Result<JobText> {
         let j: JobText = hcl::from_str(job_str)?;
-        dbg!(&j);
+        println!("--\n{:?}\n--", j);
         Ok(j)
     }
 }
 
+#[derive(Clone, Debug)]
+pub enum Task {
+    /// Producer task that generates or sources data
+    Producer(Producer),
+    /// Middle task that transforms or processes data
+    Middle(Middle),
+    /// Consumer task that consumes or stores the final data
+    Consumer(Consumer),
+}
+
 fn main() -> Result<()> {
     let j1 = JobText {
-        jtype: JobType::Fetch,
-        name: "Fetch Test JobText".to_string(),
-        source: "test_source".to_string(),
-        filters: Some(vec![Filter::Tee("test_tee".to_string()), Filter::Copy]),
+        producer: Producer::Fetch("test_source".to_string()),
+        name: "Test Job JobText".to_string(),
+        filters: Some(vec![Middle::Tee("test_tee".to_string()), Middle::Copy]),
         output: Consumer::Save("test_output.csv".to_string()),
     };
     let str = hcl::to_string(&j1)?;
-    println!("{str}");
+    println!("--\n{str}--");
 
 
     let j2 = JobText {
-        jtype: JobType::Fetch,
+        producer: Producer::Fetch("test_source".to_string()),
         name: "Fetch Test JobText".to_string(),
-        source: "test_source".to_string(),
-        filters: Some(vec![Filter::Tee("test_tee".to_string()), Filter::Copy]),
+        filters: Some(vec![Middle::Tee("test_tee".to_string()), Middle::Copy]),
         output: Consumer::Store("test_output.csv".to_string(), Freq::Daily),
     };
     let str = hcl::to_string(&j2)?;
-    println!("{str}");
+    println!("--\n{str}--");
+
+    let j3 = JobText {
+        producer: Producer::Fetch("test_source".to_string()),
+        name: "Fetch Test JobText".to_string(),
+        filters: None,
+        output: Consumer::Store("test_output.csv".to_string(), Freq::Daily),
+    };
+
+    let str = hcl::to_string(&j3)?;
+    println!("--\n{str}--");
 
     Ok(())
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -84,53 +104,56 @@ mod tests {
     #[test]
     fn parse_job_text_valid() {
         let input = r#"
-    type = "fetch"
-    name = "Test Job"
-    source = "test_source"
-    filters = [
-        { Tee = "filter_tee" },
-        "Copy",
-    ]
-    output = {
-        Save = "output.csv"
-    }
-    "#;
+name = "Test Job JobText"
+producer = {
+  fetch = "test_source"
+}
+filters = [
+  {
+    tee = "test_tee"
+  },
+  "copy"
+]
+output = {
+  save = "test_output.csv"
+}    "#;
 
         let parsed_job = JobText::parse(input);
+        dbg!(&parsed_job);
 
         assert!(parsed_job.is_ok());
         let job = parsed_job.unwrap();
 
-        assert_eq!(job.jtype, JobType::Fetch);
-        assert_eq!(job.name, "Test Job");
-        assert_eq!(job.source, "test_source");
+        assert_eq!(job.producer, Producer::Fetch("test_source".into()));
+        assert_eq!(job.name, "Test Job JobText");
         assert!(job.filters.is_some());
         let filters = job.filters.unwrap();
         assert_eq!(filters.len(), 2);
-        assert!(matches!(filters[0], Filter::Tee(ref val) if val == "filter_tee"));
-        assert!(matches!(filters[1], Filter::Copy));
-        assert_eq!(job.output, Consumer::Save("output.csv".to_string()));
+        assert!(matches!(filters[0], Middle::Tee(ref val) if val == "test_tee"));
+        assert!(matches!(filters[1], Middle::Copy));
+        assert_eq!(job.output, Consumer::Save("test_output.csv".to_string()));
     }
 
     #[test]
     fn parse_job_text_with_no_filters() {
         let input = r#"
-    type = "read"
     name = "Job Without Filters"
-    source = "source_name"
+    producer = {
+        "read" = "source_name"
+    }
     output = {
-        Save = "no_filters.csv"
+        save = "no_filters.csv"
     }
     "#;
 
         let parsed_job = JobText::parse(input);
+        dbg!(&parsed_job);
 
         assert!(parsed_job.is_ok());
         let job = parsed_job.unwrap();
 
-        assert_eq!(job.jtype, JobType::Read);
+        assert_eq!(job.producer, Producer::Read("source_name".into()));
         assert_eq!(job.name, "Job Without Filters");
-        assert_eq!(job.source, "source_name");
         assert!(job.filters.is_none());
         assert_eq!(job.output, Consumer::Save("no_filters.csv".into()));
     }
@@ -138,25 +161,27 @@ mod tests {
     #[test]
     fn parse_job_text_with_store_daily() {
         let input = r#"
-    type = "read"
     name = "Job Without Filters"
-    source = "source_name"
+    producer = {
+        "read" = "source_name"
+    }
+    filters = null
     output = {
-        "Store" = [
+        store = [
             "/tmp/somewhere",
-            "Daily"
+            "daily"
         ]
     }
     "#;
 
         let parsed_job = JobText::parse(input);
+        dbg!(&parsed_job);
 
         assert!(parsed_job.is_ok());
         let job = parsed_job.unwrap();
 
-        assert_eq!(job.jtype, JobType::Read);
+        assert_eq!(job.producer, Producer::Read("source_name".into()));
         assert_eq!(job.name, "Job Without Filters");
-        assert_eq!(job.source, "source_name");
         assert!(job.filters.is_none());
         assert_eq!(job.output, Consumer::Store("/tmp/somewhere".into(), Freq::Daily));
     }
@@ -165,11 +190,11 @@ mod tests {
     #[test]
     fn parse_job_text_invalid_type() {
         let input = r#"
-    type = "invalid"
+    producer = "invalid"
     name = "Invalid Job"
     source = "invalid_source"
     output = {
-        Save = "output.csv"
+        save = "output.csv"
     }
     "#;
 
