c64f8cc8757ef8481edda0fc4f8fb520f18d61d2
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Separate the text content (the job language) structs from what we generate.

--- engine/src/parse.rs
+++ engine/src/parse.rs
@@ -18,17 +18,19 @@
 //! output = "foo.csv"
 //! ```
 
+use std::collections::VecDeque;
+
 use eyre::Result;
+use fetiche_common::Container;
 use fetiche_formats::Format;
 use ractor::call;
 use ractor::rpc::call;
 use serde::{Deserialize, Serialize};
-use std::collections::VecDeque;
 use strum::EnumString;
 use tracing::trace;
 
 use crate::actors::{QueueMsg, SourcesMsg};
-use crate::{Engine, Fetch, Job, JobState, Read, Stream, Task};
+use crate::{Consumer, Engine, Fetch, Job, JobState, Producer, Read, Save, Store, Stream};
 
 /// Represents the type of job to be executed.
 ///
@@ -44,16 +46,16 @@ use crate::{Engine, Fetch, Job, JobState, Read, Stream, Task};
 ///
 #[derive(Clone, Debug, Deserialize, EnumString, PartialEq, Serialize)]
 #[serde(rename_all = "lowercase")]
-pub enum JobType {
+enum ProducerText {
     /// One-shot fetch a block of data.
-    Fetch,
+    Fetch(String),
     /// Read a local file.
-    Read,
+    Read(String),
     /// Long-running job, streaming.
-    Stream,
+    Stream(String),
 }
 
-/// Represents the various types of filters that can be applied to a job.
+/// Represents the various types of filters/middleware that can be applied to a job.
 ///
 /// Filters define additional processing or transformation steps to be
 /// performed on the data during a job's execution. Each filter is associated
@@ -70,7 +72,7 @@ pub enum JobType {
 /// `String` - The target or path associated with the filter action.
 ///
 #[derive(Clone, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum Filter {
+enum MiddleText {
     //// Conversion between formats.
     Convert(Format),
     /// Block by block copy.
@@ -96,7 +98,7 @@ pub enum Filter {
 /// or target destination for the consumer action.
 ///
 #[derive(Clone, Debug, Deserialize, EnumString, PartialEq, Serialize)]
-pub enum Consumer {
+enum ConsumerText {
     /// Archive multiple files in a single one.
     Archive(String),
     /// Save in a file.
@@ -131,76 +133,16 @@ pub enum Freq {
     Hourly,
 }
 
-/// Represents the main entry point for defining and executing tasks in the Fetiche engine.
-///
-/// This module provides structures and methods to define jobs, filters, and job types
-/// using the Fetiche job language. The job language is designed to allow declarative 
-/// specifications of how data is fetched, processed, and output. It supports using HCL as 
-/// the primary configuration language, while allowing flexibility to switch to JSON or 
-/// other formats in the future.
-///
-/// # Features
-///
-/// - **Job Types:** Enumerates the type of tasks that can be executed (e.g., fetching data, reading files or streaming data in real-time).
-/// - **Filters:** Allows defining additional transformation or processing applied on jobs.
-/// - **HCL Parsing:** Conversion from HCL job definitions into structured `JobStruct`.
-///
-/// # Examples
-///
-/// ## Job Definition in HCL
-///
-/// A job definition in the Fetiche language may look like:
-///
-/// ```hcl
-/// name = "Opensky"
-/// type = "fetch"
-/// source = opensky
-/// output = "foo.csv"
-/// ```
-///
-/// ## Defining and Parsing a Job in Rust
-///
-/// Here's a Rust example of defining a job with HCL and parsing it:
-///
-/// ```rust
-/// # use nom::Parser;
-/// use fetiche_engine::{Engine, JobText, JobType};
-///
-/// let hcl_input = r#"
-/// name = "Fetch Job"
-/// type = "fetch"
-/// source = "opensky"
-/// output = { "Save" = "data.csv") }
-/// "#;
-///
-/// let mut engine = Engine::new();
-/// let job: JobText = engine.parse(hcl_input).expect("Failed to parse job")?;
-///
-/// assert_eq!(job.name, "Fetch Job");
-/// assert_eq!(job.jtype, JobType::Fetch);
-/// ```
-///
-/// # Details
-///
-/// The job consists of multiple components:
-/// - **Job Types (`JobType`):** Defines the type of tasks that can be performed.
-/// - **Filters (`Filter`):** Specifies optional processing or transformation to the output.
-/// - **Job Structure (`JobStruct`):** Captures all details of the job into a structured format parsed from HCL.
-///
-/// This module makes heavy use of the `serde` crate for serialization/deserialization and the `hcl` crate for configuration parsing.
-///
 #[derive(Debug, Deserialize, Serialize)]
-pub struct JobText {
-    #[serde(rename = "type")]
-    pub jtype: JobType,
+struct JobText {
     /// Job name.
     pub name: String,
-    /// Source (aka Site name).
-    pub source: String,
+    /// Data generator
+    pub producer: ProducerText,
     /// Optional list of filters like `Tee` or `Save`.
-    pub filters: Option<Vec<Filter>>,
+    pub filters: Option<Vec<MiddleText>>,
     /// Output file name.
-    pub output: Consumer,
+    pub output: ConsumerText,
 }
 
 impl Engine {
@@ -209,39 +151,61 @@ impl Engine {
     /// - zero or more filters
     /// - one consumer
     ///
-    pub fn parse(&mut self, job_str: &str) -> Result<Job> {
+    pub async fn parse(&mut self, job_str: &str) -> Result<Job> {
         let jt: JobText = hcl::from_str(job_str)?;
         trace!("{:?}", &jt);
 
+        // Assign a new ID
+        //
+        let id = call!(self.queue, |port| QueueMsg::Allocate(port))?;
+
         // Retrieve the site's data from the Sources actor.
         //
-        let site = call!(self.sources, |port| SourcesMsg::Get(jt.name, port))?;
-        let producer = match jt.jtype {
-            JobType::Fetch => {
-                Task::from(Fetch::new(&jt.name).site(site))
+        let producer = match jt.producer {
+            ProducerText::Fetch(p) => {
+                let site = call!(self.sources, |port| SourcesMsg::Get(jt.name, port))?;
+                let f = Fetch::new(&p).site(site);
+                Producer::Fetch(f.clone())
             }
-            JobType::Stream => {
-                Task::from(Stream::new(&jt.name).site(site))
+            ProducerText::Stream(p) => {
+                let site = call!(self.sources, |port| SourcesMsg::Get(jt.name, port))?;
+                let s = Stream::new(&p).site(site);
+                Producer::Stream(s.clone())
             }
-            JobType::Read => {
-                Task::from(Read::new(&jt.name).path(&site.name))
+            ProducerText::Read(p) => {
+                let r = Read::new(&p);
+                Producer::Read(r)
             }
         };
-        let mut list = VecDeque::from([producer]);
-        if let Some(filters) = &jt.filters {
-            filters.iter().for_each(|t| list.push_back(Task::from(t)))
-        }
-        list.push_back(Task::from(jt.output));
+        let mut list = if let Some(filters) = &jt.filters {
+            filters.iter().map(|t| t.clone()).collect()
+        } else {
+            VecDeque::new()
+        };
+        let consumer = match jt.output {
+            ConsumerText::Archive(c) => {
+                Consumer::Invalid
+            }
+            ConsumerText::Save(c) => {
+                let f = Save::new(&c, Format::None, Container::Raw);
+                Consumer::Save(f)
+            }
+            ConsumerText::Store(c, f) => {
+                let s = Store::new(&c, id, f)?;
+                Consumer::Store(s)
+            }
+        };
 
-        let id = call!(self.queue, |port| QueueMsg::Allocate(port))?;
 
         // Job is now Ready as it is complete with task list.
         //
         let job = Job {
             id,
+            producer,
             name: jt.name.clone(),
             state: JobState::Ready,
-            list,
+            filters: list,
+            consumer,
         };
         Ok(job)
     }
