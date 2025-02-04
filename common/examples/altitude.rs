2b1f324a79270156843372946f28c392aeb53fc9
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Add a program to calculate the real altitude of points according to EGM2008 model.

---
+++ common/examples/altitude.rs
@@ -0,0 +1,33 @@
+use egm2008::geoid_height;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Debug, Deserialize)]
+struct Locations {
+    version: usize,
+    location: HashMap<String, Location>,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+pub struct Location {
+    /// Plus code encoded location
+    pub code: String,
+    /// Latitude
+    pub lat: f64,
+    /// Longitude
+    pub lon: f64,
+    /// Reference altitude
+    pub alt: f64,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let file_str = include_str!("../src/locations.hcl");
+
+    let list: Locations = hcl::from_str(file_str)?;
+    list.location.iter().for_each(|(n, l)| {
+        let real_h = geoid_height(l.lat as f32, l.lon as f32).unwrap();
+        println!("{}: stored altitude={:3} EGM2008={}", n, l.alt, real_h);
+    });
+
+    Ok(())
+}
