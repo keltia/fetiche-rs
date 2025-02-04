cd18c83a1f68f7ad9d5e7f98fbae23be053306e7
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Fix example with recent pyo3.

--- opensky-history/examples/version.rs
+++ opensky-history/examples/version.rs
@@ -1,3 +1,4 @@
+use pyo3::ffi::c_str;
 use pyo3::prelude::*;
 use pyo3::types::IntoPyDict;
 
@@ -7,8 +8,8 @@ fn main() -> PyResult<()> {
         let version: String = sys.getattr("version")?.extract()?;
 
         let locals = [("os", py.import("os")?)].into_py_dict(py);
-        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
+        let user: String = py.eval(code, None, Some(&locals.unwrap()))?.extract()?;
 
         println!("Hello {}, I'm Python {}", user, version);
         Ok(())
