d851f7861c514f4dfa84fb1dcedd8b6d9a00468c
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    run() code has been moved into the runner actor.

--- engine/src/job.rs
+++ engine/src/job.rs
@@ -6,13 +6,9 @@
 //! the pipe for processing.
 //!
 use std::collections::VecDeque;
-use std::io::Write;
-use std::vec;
 
 use derive_builder::Builder;
-use eyre::Result;
 use ractor::ActorRef;
-use tracing::{info, span, trace, Level};
 
 use crate::actors::StatsMsg;
 use crate::{Consumer, Middle, Producer};
@@ -79,13 +75,11 @@ pub struct Job {
 ///
 #[derive(Clone, Debug, Default, PartialEq)]
 pub enum JobState {
-    #[default]
     /// Empty, just allocated
+    #[default]
     Created,
     /// Has all its tasks
     Ready,
-    /// In the queue for next run
-    Queued,
     /// Executing
     Running,
     /// Finished
@@ -164,106 +158,6 @@ impl Job {
     pub fn state(&self) -> JobState {
         self.state.clone()
     }
-
-    /// Executes the tasks in the order they are stored in the pipeline and ensures that
-    /// the output of one task flows properly to the following task, creating a producer-consumer chain.
-    ///
-    /// # Overview
-    /// This function performs the following:
-    /// 1. Validates the job's pipeline, ensuring it has a valid start and end.
-    /// 2. Sequentially executes all tasks using their `run()` method.
-    /// 3. Writes the final task's output into the provided `out` writer.
-    ///
-    /// # Parameters
-    /// - `&mut self`: A mutable reference to the job object, enabling changes to its internal state as required.
-    /// - `out: &mut dyn Write`: A mutable reference to a writer object where the final output will be stored.
-    ///
-    /// # Returns
-    /// - `Ok(())`: Indicates success in running the pipeline to completion and writing the result to `out`.
-    /// - `Err(anyhow::Error)`: Indicates errors during pipeline validation, task execution, or IO operations.
-    ///
-    /// # Errors
-    /// This method can fail due to:
-    /// 1. Pipeline validation errors:
-    ///    - No tasks found in the pipeline.
-    ///    - Missing a starting `IO::Producer` task or ending `IO::Filter` or `IO::Consumer` task.
-    /// 2. Issues with inter-task communication or passing data between tasks.
-    /// 3. Failures while writing the final output to the provided `out` writer.
-    ///
-    /// # Notes
-    /// - Tasks in the pipeline must implement the `Runnable` trait and adhere to its constraints.
-    /// - If the execution halts due to errors, the output writer may remain unchanged.
-    /// - Useful for executing a sequence of dependent operations in a structured manner.
-    ///   `out` writer.
-    ///
-    /// # Behavior and Execution Process
-    /// 1. Logs the start of the `Job::run()` process for tracing purposes.
-    /// 2. Validates the pipeline based on these rules:
-    ///    - The first task must be a `Producer`.
-    ///    - The pipeline must have at least one task.
-    ///    - If there are multiple tasks, the last one must be a `Consumer` or a `Filter`.
-    /// 3. Creates a communication channel (`channel`) through which task messages are passed.
-    /// 4. Sequentially executes tasks using a `fold()` to chain their outputs.
-    /// 5. Sends a "start" signal to kick off the pipeline, and then gracefully closes the input channel.
-    /// 6. Collects the final output messages from the pipeline and writes them to the provided `out` writer.
-    ///
-    /// # Logging
-    /// - The function uses `tracing::span` for detailed, structured tracing of execution flow.
-    /// - High-level information is logged using `info!` (e.g., job ID, name, and task count).
-    /// - Detailed information is logged using `trace!` (e.g., pipeline creation, pipeline completion).
-    ///
-    pub fn run(&mut self, out: &mut dyn Write) -> Result<()> {
-        let span = span!(Level::TRACE, "job::run");
-        let _ = span.enter();
-
-        info!(
-            "Job({})::run({}) with {} tasks",
-            self.id,
-            self.name,
-            self.filters.len() + 2
-        );
-
-        // insert tasks into the pipeline
-        //
-        let first = vec![Task::from(self.producer.clone())];
-        let filters = self.filters.iter().map(|&t| Task::from(t)).collect::<Vec<Task>>();
-        let last = vec![Task::from(self.consumer.clone())];
-
-        // Create our list of linked tasks
-        //
-        let mut task_list = [first, filters, last].into_iter().flatten().collect::<Vec<Task>>();
-
-        // Set the pipeline up
-        //
-        let (key, stdout) = channel::<String>();
-
-        trace!("create pipeline");
-
-        // Gather results for all tasks into a single pipeline using `Iterator::fold()`
-        //
-        let output = task_list.iter_mut().fold(stdout, |acc, t| {
-            let (rx, _h) = t.run(acc);
-            rx
-        });
-
-        trace!("starting pipe");
-
-        // Start the pipeline
-        //
-        key.send("start".to_string())?;
-
-        // Close the pipeline which will stop all threads in sequence
-        //
-        drop(key);
-
-        // Wait for final output to be received and send it out
-        //
-        for msg in output {
-            write!(out, "{}", msg)?;
-        }
-        trace!("pipe finished.");
-        Ok(out.flush()?)
-    }
 }
 
 #[cfg(test)]
@@ -280,7 +174,7 @@ mod tests {
 
         assert_eq!(job.name, "Test Job");
         assert_eq!(job.id, 1);
-        assert!(job.filters.is_empty());
+        assert!(job.middle.is_empty());
     }
 
     #[test]
@@ -292,7 +186,7 @@ mod tests {
             .unwrap();
 
         assert_eq!(job.name, "empty");
-        assert!(job.filters.is_empty());
+        assert!(job.middle.is_empty());
     }
 
     #[test]
@@ -313,13 +207,13 @@ mod tests {
     #[test]
     fn test_job_add_filters() {
         let mut job = JobBuilder::default().id(1).build().unwrap();
-        assert!(job.filters.is_empty());
+        assert!(job.middle.is_empty());
 
         job.add(Middle::Invalid);
-        assert_eq!(job.filters.len(), 1);
+        assert_eq!(job.middle.len(), 1);
 
         job.add(Middle::Invalid);
-        assert_eq!(job.filters.len(), 2);
+        assert_eq!(job.middle.len(), 2);
     }
 
     #[test]
