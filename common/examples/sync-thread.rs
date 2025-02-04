ad124f5b59768d85819ebb92d5c1bbdc6b206954
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Move sync-thread into common.

--- common/examples/sync-thread.rs
+++ common/examples/sync-thread.rs
@@ -2,33 +2,19 @@
 //
 
 use std::io::{stderr, Write};
-use std::sync::atomic::AtomicBool;
 use std::sync::mpsc;
-use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 
 use eyre::Result;
-use signal_hook::consts::TERM_SIGNALS;
-use signal_hook::flag;
+use fetiche_common::{close_logging, init_logging};
+use tracing::trace;
 
 // If 0, infinite wait, need SIGINT to sop
 //
 const SLEEP: u64 = 20;
 
 fn worker_thread(out: &mut dyn Write, d: u64) -> Result<()> {
-    let term = Arc::new(AtomicBool::new(false));
-
-    // Setup signals
-    //
-    // NOTE: SIGINT must be issued twice to immediately stop, not sure is it needed.
-    //
-    // NOTE: on Windows, single register does not work, picture me surprised (not)
-    //
-    for sig in TERM_SIGNALS {
-        flag::register_conditional_shutdown(*sig, 1, Arc::clone(&term))?;
-        flag::register(*sig, Arc::clone(&term))?;
-    }
 
     // Launch it!
     //
@@ -79,8 +65,17 @@ fn worker_thread(out: &mut dyn Write, d: u64) -> Result<()> {
 async fn main() -> Result<()> {
     let mut out = stderr();
 
+    let _ = init_logging("sync-thread", false, true, None);
+
+    let _ = ctrlc::set_handler(move || {
+        trace!("Ctrl-C pressed");
+        close_logging();
+        std::process::exit(0);
+    });
+
     worker_thread(&mut out, SLEEP)?;
 
     println!("with sleeper, nothing is displayed");
+    close_logging();
     std::process::exit(0);
 }
