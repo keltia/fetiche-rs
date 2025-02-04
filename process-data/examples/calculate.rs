d75ba30c5620486fa03eae81ee8d58f06e3e830f
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Simplify the enum_dispatch example.

--- process-data/examples/calculate.rs
+++ process-data/examples/calculate.rs
@@ -1,14 +1,15 @@
+use async_trait::async_trait;
 use enum_dispatch::enum_dispatch;
-use std::fmt::Debug;
-
 use eyre::Result;
 use rand::Rng;
+use std::fmt::Debug;
 
 /// This trait define an object that can be calculated
 ///
+#[async_trait]
 #[enum_dispatch(Task)]
 pub trait Calculate: Debug {
-    fn execute(&self) -> Stat;
+    async fn execute(&self) -> Stat;
 }
 
 // -----
@@ -21,34 +22,14 @@ pub enum Stat {
 
 // -----
 
-#[derive(Debug)]
-pub struct Batch {
-    inner: Vec<Task>,
-}
-
-impl Batch {
-    pub fn new() -> Self {
-        Self { inner: vec![] }
-    }
-
-    pub fn add(&mut self, task: Task) -> &mut Self {
-        let _ = &self.inner.push(task);
-        self
-    }
-
-    pub fn run(&mut self) -> Vec<Stat> {
-        let res: Vec<Stat> = self.inner.iter().map(|e| e.execute()).collect();
-        eprintln!("res={:?}", res);
-        res
-    }
-}
-
 #[enum_dispatch]
 #[derive(Clone, Debug)]
 pub enum Task {
     Foo,
     Bar,
 }
+
+// -----
 
 #[derive(Clone, Debug)]
 pub struct Foo {
@@ -61,14 +42,17 @@ impl Foo {
     }
 }
 
+#[async_trait]
 impl Calculate for Foo {
-    fn execute(&self) -> Stat {
+    async fn execute(&self) -> Stat {
         let mut rng = rand::rng();
         let res: u32 = rng.random();
         let res = res / self.m;
         Stat::One(res)
     }
 }
+
+// -----
 
 #[derive(Clone, Debug)]
 pub struct Bar {
@@ -81,8 +65,9 @@ impl Bar {
     }
 }
 
+#[async_trait]
 impl Calculate for Bar {
-    fn execute(&self) -> Stat {
+    async fn execute(&self) -> Stat {
         let mut rng = rand::rng();
         let res: f64 = rng.random();
         let res = res / self.f;
@@ -90,22 +75,20 @@ impl Calculate for Bar {
     }
 }
 
-fn main() -> Result<()> {
+// -----
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let t1 = Foo::new(2);
     let t2 = Bar::new(4.0);
 
     let c1 = Task::from(t1);
     let c2 = Task::from(t2);
 
-    let r1 = c1.execute();
-    let r2 = c2.execute();
+    let r1 = c1.execute().await;
+    let r2 = c2.execute().await;
 
     dbg!(r1, r2);
 
-    let mut b = Batch::new();
-    b.add(c1.clone()).add(c2.clone());
-
-    let res: Vec<Stat> = b.run();
-    dbg!(&res);
     Ok(())
 }
