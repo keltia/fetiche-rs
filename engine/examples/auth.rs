facf0426f3902a78b345f7650d8bf8ba5d5816b8
Ollivier Robert <roberto@keltia.net>
2025-02-04 22:26:03 +0100

    Simplify.

--- engine/examples/auth.rs
+++ engine/examples/auth.rs
@@ -1,6 +1,5 @@
 use fetiche_common::init_logging;
 use fetiche_engine::Engine;
-use tokio::task;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -9,7 +8,7 @@ async fn main() -> eyre::Result<()> {
     let engine = Engine::new().await;
     dbg!(&engine);
 
-    let str = task::spawn_blocking(move || engine.list_tokens().unwrap()).await?;
+    let str = engine.list_tokens()?;
     eprintln!("{}", str);
     Ok(())
 }
