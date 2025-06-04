1e4b96e4df56dd0b0ce611cf21bd9b3429bc9dc3
Ollivier Robert <roberto@keltia.net>
2025-06-04 21:44:25 +0200

    Finish engine restructuring.

--- engine/src/sources/access/opensky/device.rs
+++ engine/src/sources/access/opensky/device.rs
@@ -1,18 +1,34 @@
 //! This is the streaming implementation for Opensky, direct connection to the antenna.
 //!
+//! There are different ports available, each with a specific output format:
+//!
+//! - 30001: RAW TCP
+//! - 30002: Raw MODE-S frames
+//! - 30003: CSV
+//! - 30004:
+//! - 30005: Binary format
+//!
 //! FIXME: this is not using an actor
 
-use crate::{AuthError, Capability, Stats, Streamable};
-use fetiche_formats::Format;
-use serde::Serialize;
 use std::sync::mpsc::Sender;
 
-/// Default port for listening
-const DEF_PORT: u16 = 30005;
+use eyre::Result;
+use nom::Parser;
+use ractor::ActorRef;
+use serde::{Deserialize, Serialize};
+use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::net::TcpStream;
+
+use crate::actors::StatsMsg;
+use crate::{AuthError, Capability, ParamError, Site, Stats, Streamable};
+use fetiche_formats::Format;
+
+/// Default port for data, we prefer the straight CSV output
+const DEF_PORT: u16 = 30003;
 
 /// This is the struct holding potential parameters to the API
 ///
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 struct Param {
     /// IP of the receiver antenna
     ///
@@ -22,36 +38,48 @@ struct Param {
 }
 
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct OpenskyDevice {
-    /// Describe the different features of the source
-    pub feature: Capability,
     /// TCP stream
-    pub stream: Option<std::net::TcpStream>,
+    stream: Option<String>,
+    /// Stats actor ref
+    stats: Option<ActorRef<StatsMsg>>,
 }
 
 impl OpenskyDevice {
+    #[tracing::instrument]
     pub fn new() -> Self {
         Self {
-            feature: Capability::Stream,
             stream: None,
+            stats: None,
         }
     }
 
+    #[tracing::instrument(skip(self))]
+    pub fn load(&mut self, _site: Site) -> &mut Self { self }
+
+    #[tracing::instrument(skip(self))]
     pub fn connect(&mut self, name: &str) -> &mut Self {
-        self.stream = Some(std::net::TcpStream::connect(name).unwrap());
+        self.stream = Some(name.to_string());
         self
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub fn stats(&mut self, stats: ActorRef<StatsMsg>) -> &mut Self {
+        self.stats = Some(stats);
+        self
+    }
+
+    #[tracing::instrument(skip(self))]
+    pub fn feature(&self) -> Result<Capability> {
+        Ok(Capability::Stream)
     }
 }
 
 impl Streamable for OpenskyDevice {
     #[tracing::instrument(skip(self))]
-    fn format(&self) -> Format {
-        Format::Opensky
-    }
-
-    #[tracing::instrument(skip(self))]
     fn name(&self) -> String {
-        String::new("openskydevice")
+        String::from("openskydevice")
     }
 
     /// Fake function.
@@ -61,8 +89,42 @@ impl Streamable for OpenskyDevice {
         Ok(String::new())
     }
 
-    #[tracing::instrument(skip(self, out, token))]
-    async fn stream(&self, out: Sender<String>, token: &str, args: &str) -> eyre::Result<Stats> {
-        todo!()
+    #[tracing::instrument(skip(self, out, _token))]
+    async fn stream(&self, out: Sender<String>, _token: &str, _args: &str) -> Result<Stats> {
+        //let args: Param = serde_json::from_str(args)?;
+
+        let tag = self.name().clone();
+        let stat = self.stats.clone().ok_or(Err(ParamError::NoStatsActor).into())?;
+
+        stat.cast(StatsMsg::Reset(tag.clone()))?;
+
+        // Get addr from parameters and connect
+        //
+        let addr = self.stream.clone().ok_or(Err(ParamError::NoAddrGiven).into())?;
+        let flow = TcpStream::connect(addr).await?;
+        let mut flow = BufReader::new(flow).lines();
+        let mut st = Stats::default();
+
+        // Read every line until… whatever
+        //
+        while let Some(line) = flow.next_line().await? {
+            let len = line.len();
+            st.pkts += 1;
+            st.bytes = st.bytes + len as u64;
+            let _ = out.send(line.clone())?;
+        };
+
+        stat.cast(StatsMsg::Update(tag, Stats {
+            bytes: st.bytes,
+            pkts: st.pkts,
+            ..Default::default()
+        }))?;
+
+        Ok(st)
+    }
+
+    #[tracing::instrument(skip(self))]
+    fn format(&self) -> Format {
+        Format::Opensky
     }
 }
