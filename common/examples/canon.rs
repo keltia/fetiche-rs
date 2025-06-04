3e45bfde27fb0325346369765add148859a72392
Ollivier Robert <roberto@keltia.net>
2025-06-04 21:44:25 +0200

    Difference between canonicalize() and absolute().

--- common/examples/canon.rs
+++ common/examples/canon.rs
@@ -1,15 +1,28 @@
+use eyre::Result;
 use std::env;
 use std::fs::canonicalize;
-use std::path::absolute;
-
-use eyre::Result;
-use tracing::debug;
+use std::path::{absolute, PathBuf};
 
 fn main() -> Result<()> {
     let fname = env::args().nth(1).ok_or(eyre::eyre!("missing output path"))?;
-    debug!("output path: {:?}", fname);
-    let fname = canonicalize(fname)?;
-    let fname = env::current_dir()?.join(fname);
-    println!("{}", fname.to_string_lossy().to_string());
+    println!("input path: {:?}", fname);
+
+    let name = match canonicalize(&fname) {
+        Ok(fname) => fname,
+        Err(err) => {
+            PathBuf::from(err.to_string())
+        }
+    };
+    println!("canonicalized path: {:?}", name);
+
+    let fname = match absolute(&fname) {
+        Ok(fname) => fname,
+        Err(err) => {
+            PathBuf::from(err.to_string())
+        }
+    };
+    println!("absolute path: {:?}", fname);
+
+
     Ok(())
 }
