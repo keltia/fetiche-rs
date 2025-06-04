5a96985f1aa6f4980c95327bdd2e12871d0b16e2
Ollivier Robert <roberto@keltia.net>
2025-06-04 21:44:25 +0200

    Use the standard block_box() now.

--- process-data/benches/distances.rs
+++ process-data/benches/distances.rs
@@ -69,8 +69,11 @@
 //! - `tokio`: For managing asynchronous operations.
 //! - `std::env`: For environment variable retrieval in the `klickhouse` benchmark.
 //!
+
+use std::hint::black_box;
+
 use criterion::async_executor::FuturesExecutor;
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use geo::point;
 use geo::prelude::*;
 use klickhouse::{Client, ClientOptions, QueryBuilder, RawRow};
