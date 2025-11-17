bd49490a553f054294ddd63691f14f695c5a267c
Ollivier Robert <roberto@keltia.net>
2025-11-17 15:35:48 +0100

    Change avionix-pipeline to import JSONL directly to avoid column sorting.

--- scripts/avionix-pipeline.sh
+++ scripts/avionix-pipeline.sh
@@ -5,6 +5,8 @@
 # If `.` is specified as filename, try to compute last day as YYYYMMDD and use it as `BNAME`
 #
 # FIXME This should be python (or better), not a dumb shell script
+#
+# NOTE CLICKHOUSE_* variables MUST be defined at run-time.
 
 # Base config
 BASEDIR="/acute"
@@ -27,11 +29,14 @@ MONTH=$(date +%m)
 YEAR=$(date +%Y)
 
 # First step — convert into csv & parquet
+#
+# FIXME columns order will be sorted, this is a datafusion BUG
+#
 sort -u "${BNAME}.json" > "${BNAME}s.json" && \
-	bdt convert -s "${BNAME}s.json" "${BNAME}.csv" && \
-	bdt convert -s "${BNAME}.csv" "${BNAME}.parquet"
+	bdt convert -s "${BNAME}s.json" "${BNAME}.parquet"
 
 # Second step — create our archive tree
+#
 [[ ! -d "${DATADIR}/year=${YEAR}/month=${MONTH}" ]] && \
 	mkdir -p "${DATADIR}/year=${YEAR}/month=${MONTH}"
 
@@ -39,5 +44,11 @@ sort -u "${BNAME}.json" > "${BNAME}s.json" && \
 mv "${BNAME}.parquet" "${DATADIR}/year=${YEAR}/month=${MONTH}/"
 
 # Third step b  — import
-${BASEDIR}/bin/import-avionix.py -D ${BASEDIR} -d "${BNAME}.csv" && \
-	rm -f "${BNAME}s.json" "${BNAME}.json" "${BNAME}.csv" 
+#
+#${BASEDIR}/bin/import-avionix.py -D ${BASEDIR} -d "${BNAME}.csv" && \
+#	rm -f "${BNAME}s.json" "${BNAME}.json" "${BNAME}.csv"
+
+cat "${BNAME}s.json" | clickhouse-client -h $CLICKHOUSE_HOST -u $CLICKHOUSE_USER \
+  --password $CLICKHOUSE_PASSWD -d $CLICKHOUSE_DB \
+  -q 'INSERT INTO avionix_raw FORMAT JSONEachRow' && \
+  rm -f "${BNAME}s.json" "${BNAME}.json"
