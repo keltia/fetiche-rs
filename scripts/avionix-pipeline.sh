f3ed330abfbbf5e0c7658f0361614b7c93205e55
Ollivier Robert <roberto@keltia.net>
2025-11-24 11:12:47 +0100

    Adapt avionix-pipeline.sh to use convert-from-json.

--- scripts/avionix-pipeline.sh
+++ scripts/avionix-pipeline.sh
@@ -9,11 +9,22 @@
 # NOTE CLICKHOUSE_* variables MUST be defined at run-time.
 
 # Base config
+#
 BASEDIR="/acute"
 DATADIR="${BASEDIR}/data/avionix"
 
+# Check where we are running
+#
+PLATFORM=$(uname)
+if [ x"${PLATFORM}" = x"Darwin" ]; then
+  CLIENT="clickhouse client"
+else
+  CLIENT="clickhouse-client"
+fi
+
 # Check arguments
-ARG=$1;
+#
+ARG=$1
 if [ x"$ARG" = x"." ]; then
   BNAME=$(date +"%Y%m%d" -d yesterday)
 else
@@ -21,7 +32,8 @@ else
 fi
 
 # Check file exists
-[[ ! -f "${BNAME}.json" ]] && exit 1
+#
+[[ ! -f "${BNAME}.json" ]] && echo "No json" && exit 1
 #
 echo "Cwd is ${PWD}"
 echo "Basename is ${BNAME}"
@@ -29,31 +41,31 @@ echo "Basename is ${BNAME}"
 MONTH=$(date +%m)
 YEAR=$(date +%Y)
 
-# First step — convert into csv & parquet
-#
-# FIXME columns order will be sorted, this is a datafusion BUG
+# First step — convert into parquet
 #
 echo "Convert to parquet"
 sort -u "${BNAME}.json" > "${BNAME}s.json" && \
-	bdt convert -s "${BNAME}s.json" "${BNAME}.parquet"
+  mv "${BNAME}s.json" "${BNAME}.json" &&  \
+	${BASEDIR}/bin/convert-from-json -P "${BNAME}.json"
+
+[[ ! -f "${BNAME}.parquet" ]] && echo "no parquet" && exit 1
 
 # Second step — create our archive tree
 #
 echo "Create dir tree"
-[[ ! -d "${DATADIR}/year=${YEAR}/month=${MONTH}" ]] && \
-	mkdir -p "${DATADIR}/year=${YEAR}/month=${MONTH}"
+DESTDIR = "${DATADIR}/year=${YEAR}/month=${MONTH}"
+[[ ! -d "${DESTDIR}" ]] && \
+	mkdir -p "${DESTDIR}"
 
 # Third step a  — move
 echo "Move files."
-mv "${BNAME}.parquet" "${DATADIR}/year=${YEAR}/month=${MONTH}/"
+mv "${BNAME}.parquet" "${DESTDIR}/${BNAME}.parquet"
 
 # Third step b  — import
 #
-#${BASEDIR}/bin/import-avionix.py -D ${BASEDIR} -d "${BNAME}.csv" && \
-#	rm -f "${BNAME}s.json" "${BNAME}.json" "${BNAME}.csv"
 echo "Insert into CH."
-cat "${BNAME}s.json" | clickhouse-client -h $CLICKHOUSE_HOST -u $CLICKHOUSE_USER \
+cat "${BNAME}.json" | ${CLIENT} -h $CLICKHOUSE_HOST -u $CLICKHOUSE_USER \
   --password $CLICKHOUSE_PASSWD -d $CLICKHOUSE_DB \
   -q 'INSERT INTO avionix_raw FORMAT JSONEachRow' && \
-  rm -f "${BNAME}s.json" "${BNAME}.json"
+  rm -f "${BNAME}.json"
 [[ $? == 0 ]] && echo "End."
