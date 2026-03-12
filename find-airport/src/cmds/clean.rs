f06042517ff9a5b7a0083cd3731ad279f4bb58b1
Ollivier Robert <roberto@keltia.net>
2026-03-12 11:00:07 +0100

    Handle the list of sources.  Renamed cmd_clean() for consistency.

--- find-airport/src/cmds/clean.rs
+++ find-airport/src/cmds/clean.rs
@@ -1,5 +1,5 @@
 use std::env::set_current_dir;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use eyre::Result;
 use tokio::fs;
@@ -8,31 +8,36 @@ use tracing::info;
 use crate::runtime::Context;
 
 #[tracing::instrument]
-pub async fn clean(ctx: &Context) -> Result<()> {
+pub async fn cmd_clean(ctx: &Context) -> Result<()> {
     info!("Cleaning up airport data files and resources");
-
-    // Get our filename
-    //
-    let fname = ctx.cfg["file"].clone();
-    let fname = Path::new(&fname);
-
-    let basename = fname.file_stem().unwrap().to_str().unwrap();
-
-    let parquet = Path::new(&basename).with_extension("parquet");
 
     // Check the current file mtime (if it exists)
     //
     let target_dir = Path::new(&ctx.cfg["datalake"]).join("files");
-
-    // Let us move into the final destination
-    //
     set_current_dir(&target_dir)?;
 
-    if parquet.exists() {
-        info!("Deleting file: {:?}", parquet);
-        Ok(fs::remove_file(&parquet).await?)
-    } else {
-        info!("No file to delete");
-        Ok(())
+    // Get our filenames
+    //
+    let sources = ctx.cfg["sources"]
+        .clone()
+        .split(",")
+        .map(|s| s.to_owned())
+        .collect::<Vec<_>>();
+
+    for fname in sources.into_iter() {
+        let basename = Path::new(&fname);
+        let _ = clean_one(&basename).await?;
     }
+    Ok(())
+}
+
+#[tracing::instrument]
+async fn clean_one(fname: &Path) -> Result<()> {
+    let parquet = fname.with_extension("parquet");
+    if parquet.exists() {
+        info!("clean_one file={:?}", parquet);
+        return Ok(fs::remove_file(&parquet).await?);
+    }
+    info!("clean_one err=no file");
+    Ok(())
 }
