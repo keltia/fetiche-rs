07d4ed16466e5eb23140351a1b3b0a4e2186fb43
Ollivier Robert <roberto@keltia.net>
2026-07-26 23:33:28 +0200

    Add header info with results, cleanup imports.

--- process-data/benches/format.rs
+++ process-data/benches/format.rs
@@ -1,7 +1,19 @@
+//! Micro-benchmark for strftime/format between chrono and jiff
+//!
+//! PC, AMD 7700X, 32 GB, 1 TB M2 SSD, Win11 25H2
+//! ```text
+//! Timer precision: 100 ns
+//! format                   fastest       │ slowest       │ median        │ mean          │ samples │ iters
+//! ├─ test_chrono_format    128.7 ns      │ 163 ns        │ 130.2 ns      │ 131.4 ns      │ 100     │ 12800
+//! ├─ test_chrono_strftime  218.5 ns      │ 315.4 ns      │ 231 ns        │ 236.6 ns      │ 100     │ 6400
+//! ├─ test_jiff_format      120.1 ns      │ 252.1 ns      │ 127.1 ns      │ 128.6 ns      │ 100     │ 12800
+//! ╰─ test_jiff_strftime    85.74 ns      │ 88.87 ns      │ 86.13 ns      │ 86.27 ns      │ 100     │ 12800
+//! ```
+//!
+
 use std::hint::black_box;
 
 use divan::Bencher;
-use jiff::Zoned;
 
 fn main() {
     divan::main();
@@ -23,7 +35,7 @@ fn test_chrono_format(bencher: Bencher) {
 
 #[divan::bench]
 fn test_chrono_strftime(bencher: Bencher) {
-    use chrono::{DateTime, Datelike, Utc};
+    use chrono::{DateTime, Utc};
 
     fn strftime_day(day: &DateTime<Utc>) -> String {
         day.format("%Y-%m-%d").to_string()
@@ -37,6 +49,8 @@ fn test_chrono_strftime(bencher: Bencher) {
 
 #[divan::bench]
 fn test_jiff_format(bencher: Bencher) {
+    use jiff::Zoned;
+
     fn format_day(day: &Zoned) -> String {
         format!("{:04}-{:02}-{:02}", day.year(), day.month(), day.day()).to_string()
     }
@@ -49,6 +63,8 @@ fn test_jiff_format(bencher: Bencher) {
 
 #[divan::bench]
 fn test_jiff_strftime(bencher: Bencher) {
+    use jiff::Zoned;
+
     fn strftime_day(day: &Zoned) -> String {
         day.strftime("%Y-%m-%d").to_string()
     }
